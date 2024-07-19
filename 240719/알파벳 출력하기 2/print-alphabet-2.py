e042cd91699c4f5dc1d8b547ab4ec29e859c92fc
jelly <sohyunzzq@naver.com>
2024-07-19 18:13:06 +0900

    Title: 알파벳 출력하기 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240719/알파벳 출력하기 2/print-alphabet-2.py
@@ -0,0 +1,11 @@
+str1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+n = int(input())
+index = 0
+
+for i in range(n):
+    for j in range(i):
+        print("  ", end = "")
+    for j in range(n-i):
+        print(str1[index], end = " ")
+        index += 1
+    print()

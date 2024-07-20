43955eb4605fd9de224162e8b31abec1f144c645
jelly <sohyunzzq@naver.com>
2024-07-21 01:11:30 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 103ms, Memory: 24MB, Status: Passed - CodeTree

--- 240721/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
+++ 240721/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
@@ -1,6 +1,6 @@
 def check(a, b):
     for i in range(len(a)):
-        if i > len(b):
+        if i > len(a) - len(b):
             return False
         if a[i:i+len(b)] == b:
             return True

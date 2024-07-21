d3db3200f97ae8f031a0676e6b10b25afc8e62f9
jelly <sohyunzzq@naver.com>
2024-07-22 00:59:54 +0900

    Title: 중앙값 계산 2, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 240722/중앙값 계산 2/get-median-2.py
+++ 240722/중앙값 계산 2/get-median-2.py
@@ -2,8 +2,8 @@ n = int(input())
 
 lst = list(map(int, input().split()))
 lst2 = []
-for i in lst:
-    lst2.append(i)
+for i in range(1, n+1):
+    lst2.append(lst[i-1])
     if i % 2 == 1:
         lst3 = sorted(lst2)
         print(lst3[len(lst3)//2], end = " ")

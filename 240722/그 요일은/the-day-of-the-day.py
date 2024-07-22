e8483f22e8a91edae44786e2cc28407852530c38
jelly <sohyunzzq@naver.com>
2024-07-22 22:38:54 +0900

    Title: 그 요일은, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240722/그 요일은/the-day-of-the-day.py
@@ -0,0 +1,26 @@
+month = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+day = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+
+m1, d1, m2, d2 = map(int, input().split())
+A = input()
+
+for i in range(6):
+    if A == day[i]:
+        index = i
+
+old = 0
+new = 0
+
+for i in range(m1):
+    old += month[i]
+old += d1
+
+for i in range(m2):
+    new += month[i]
+new += d2
+
+ans = (new-old) // 7
+if index <= (new-old) % 7:
+    ans += 1
+
+print(ans)

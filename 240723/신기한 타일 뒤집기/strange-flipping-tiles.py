4d07bf98b9a350c85e5c073ec7a515aaeac5f194
jelly <sohyunzzq@naver.com>
2024-07-23 03:48:55 +0900

    Title: 신기한 타일 뒤집기, Time: 148ms, Memory: 27MB, Status: Wrong Answer - CodeTree

---
+++ 240723/신기한 타일 뒤집기/strange-flipping-tiles.py
@@ -0,0 +1,34 @@
+n = int(input())
+lst = [0] * (n * 100 * 2 + 1)
+
+now = n * 100
+
+for i in range(n):
+    num, pos = input().split()
+    if pos == "L":
+        for j in range(now - int(num) + 1, now + 1):
+            lst[j] = 1
+        now = now - int(num)
+    else:
+        for j in range(now, now + int(num)):
+            lst[j] = 2
+        now = now + int(num) - 1
+
+w = 0
+b = 0
+for i in lst:
+    if i == 1:
+        w += 1
+    elif i == 2:
+        b += 1
+
+print(w, b)
+'''
+흰 1 검 2
+0 0 0 0 0 0 0 0 0 0
+        s
+        2 2 2 2
+      1 1 1 1 1
+      2 2 2 2 2 2 2
+            1 1 1 1  
+'''

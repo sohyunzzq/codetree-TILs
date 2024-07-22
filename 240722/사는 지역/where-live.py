f13fb7e5319d9f915dae145935e58ba455eb4b3d
jelly <sohyunzzq@naver.com>
2024-07-22 17:38:48 +0900

    Title: 사는 지역, Time: 203ms, Memory: 24MB, Status: Passed - CodeTree

--- 240722/사는 지역/where-live.py
+++ 240722/사는 지역/where-live.py
@@ -11,8 +11,8 @@ for i in range(n):
     name, num, city = input().split()
     people.append(City(name, num, city))
 
-people.sort(key = lambda x: x.name)
+people.sort(key = lambda x: x.name, reverse=True)
 
-print("name {}".format(people[2].name))
-print("addr {}".format(people[2].num))
-print("city {}".format(people[2].city))
+print("name {}".format(people[0].name))
+print("addr {}".format(people[0].num))
+print("city {}".format(people[0].city))

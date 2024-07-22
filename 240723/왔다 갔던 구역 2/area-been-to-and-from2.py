7c177399149ca075f7a29e12445afcda4d2e2159
jelly <sohyunzzq@naver.com>
2024-07-23 01:50:32 +0900

    Title: 왔다 갔던 구역 2, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

--- 240723/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 240723/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -5,23 +5,20 @@ n = int(input())
 10번 왼쪽으로 간다면
 
 - 10*n ~ 10*n 이 최대 구간임
-: 0 ~ 20*n을 구간으로 잡고,
-만약 n=1일 때는
--10 ~ 10인데
-중간이 0
-0 ~ 20, 중간이 10
+: 0 ~ 20*n
 
+수직선, 겹치는 구간 → 끝나는 구간 안 더해줌
 10 L 이 들어오면
-현재 위치부터 10칸 왼쪽까지 다 1을 더해줌
-now부터 now-10까지
+0, 10 이므로 0 ~ 9까지 1 더해줌
+그 다음 now는 0이 됨
 '''
-area = [0] * (10*n*2+1)
+area = [0] * (20*n+1)
 now = 10*n
 
 for i in range(n):
     num, p = input().split()
     if p == "L":
-        for j in range(now, now-int(num), -1):
+        for j in range(now-int(num), now):
             area[j] += 1
         now -= int(num)
     else:

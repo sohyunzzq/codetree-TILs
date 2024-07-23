287fc16f91999083011e2d58eeb2f6ca82ba6221
jelly <sohyunzzq@naver.com>
2024-07-23 20:52:11 +0900

    Title: 연속되는 수 2, Time: 137ms, Memory: 24MB, Status: Passed - CodeTree

--- 240723/연속되는 수 2/continuous-number2.py
+++ 240723/연속되는 수 2/continuous-number2.py
@@ -8,10 +8,9 @@ cnt = 1
 res = 0
 for i in range(n):
     if i == 0 or lst[i] != lst[i-1]:
-        res = max(cnt, res)
         cnt = 1
     else:
         cnt += 1
-res = max(cnt, res)
+    res = max(cnt, res)
 
 print(res)

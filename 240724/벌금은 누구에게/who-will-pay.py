4acf01044f8b058a44efc68508191bd69d0f23cd
jelly <sohyunzzq@naver.com>
2024-07-24 00:48:49 +0900

    Title: 벌금은 누구에게, Time: 127ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240724/벌금은 누구에게/who-will-pay.py
+++ 240724/벌금은 누구에게/who-will-pay.py
@@ -1,9 +1,3 @@
-def check(k):
-    for i in range(n+1):
-        if lst[i] >= k:
-            return i
-    return -1
-
 n, m, k = map(int, input().split())
 
 lst = [0] * (n+1)
@@ -12,10 +6,11 @@ num = []
 for i in range(m):
     num.append(int(input()))
 
+result = -1
 for i in range(m):
     lst[num[i]] += 1
-    result = check(k)
-    if result > 0:
+    if lst[num[i]] >= 3:
+        result = num[i]
         break
 
 print(result)

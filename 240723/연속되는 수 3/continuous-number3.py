54606f2d0006650158f7d83f54a2f06c6d33f972
jelly <sohyunzzq@naver.com>
2024-07-23 21:12:58 +0900

    Title: 연속되는 수 3, Time: 125ms, Memory: 24MB, Status: Passed - CodeTree

--- 240723/연속되는 수 3/continuous-number3.py
+++ 240723/연속되는 수 3/continuous-number3.py
@@ -2,19 +2,15 @@ n = int(input())
 
 lst = []
 for i in range(n):
-    a = int(input())
-    if a > 0:
-        lst.append(1)
-    else:
-        lst.append(-1)
+    lst.append(int(input()))
 
 cnt = 1
 res = 0
 for i in range(n):
-    if i == 0 or lst[i] != lst[i-1]:
-        cnt = 1
-    else:
+    if i >= 1 and lst[i] * lst[i-1] > 0: #같은 부호
         cnt += 1
+    else:
+        cnt = 1
     res = max(res, cnt)
 
 print(res)

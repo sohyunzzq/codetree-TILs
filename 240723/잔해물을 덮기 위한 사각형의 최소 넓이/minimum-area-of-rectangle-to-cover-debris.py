2fd23feef0bbf8ea5c4a15fb5695c67479303df6
jelly <sohyunzzq@naver.com>
2024-07-23 20:14:33 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 210ms, Memory: 55MB, Status: Wrong Answer - CodeTree

--- 240723/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
+++ 240723/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -13,48 +13,15 @@ for i in range(2001):
 cover(r1x1+1000, r1y1+1000, r1x2+1000, r1y2+1000, 1)
 cover(r2x1+1000, r2y1+1000, r2x2+1000, r2y2+1000, 0)
 
-a = -1
-b = -1
-c = -1
-d = -1
+result = 0
 
-flag = 0
+xmax, xmin, ymax, ymin = 0, 0, 0, 0
 for i in range(2001):
-    if flag == 0:
-        for j in range(2001):
-            if area[i][j] == 1:
-                a = i
-                flag = 1
-                break
-
-flag = 0
-for i in range(2000, -1, -1):
-    if flag == 0:
-        for j in range(2001):
-            if area[i][j] == 1:
-                b = i
-                flag = 1
-                break
-
-flag = 0
-for i in range(2001):
-    if flag == 0:
-        for j in range(2001):
-            if area[j][i] == 1:
-                c = i
-                flag = 1
-                break
-
-flag = 0
-for i in range(2000, -1, -1):
-    if flag == 0:
-        for j in range(2001):
-            if area[j][i] == 1:
-                d = i
-                flag = 1
-                break
-
-if a == -1 or b == -1 or c == -1 or d == -1:
-    print(0)
-else:
-    print((b-a+1) * (d-c+1))
+    for j in range(2001):
+        if area[i][j] == 1:
+            xmax = max(i, xmax)
+            xmin = min(i, xmin)
+            ymax = max(j, ymax)
+            ymin = min(j, ymin)
+            result = (xmax - xmin + 1) * (ymax - ymin + 1)
+print(result)

5b4f34806bf4a66da64ceb29b09be5f7fc53c0ac
jelly <sohyunzzq@naver.com>
2024-07-23 22:04:17 +0900

    Title: 만나는 그 순간, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240723/만나는 그 순간/the-moment-we-meet.py
@@ -0,0 +1,34 @@
+n, m = map(int, input().split())
+
+A = []
+B = []
+Anow, Bnow = 0, 0
+
+for i in range(n):
+    pos, num = input().split()
+    
+    if pos == "L":
+        for j in range(Anow - 1, Anow - int(num) - 1, -1):
+            A.append(j)
+        Anow -= int(num)
+    else:
+        for j in range(Anow + 1, Anow + int(num) + 1):
+            A.append(j)
+        Anow += int(num)
+
+for i in range(m):
+    pos, num = input().split()
+    
+    if pos == "L":
+        for j in range(Bnow - 1, Bnow - int(num) - 1, -1):
+            B.append(j)
+        Bnow -= int(num)
+    else:
+        for j in range(Bnow + 1, Bnow + int(num) + 1):
+            B.append(j)
+        Bnow += int(num)
+
+for i in range(1, min(len(A), len(B))):
+    if A[i] == B[i]:
+        print(i+1)
+        break

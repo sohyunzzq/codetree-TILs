b9ff946075b637219d8bfc34aeb408d9712ca93e
jelly <sohyunzzq@naver.com>
2024-07-24 13:03:03 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240724/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 240724/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -13,10 +13,10 @@ lst.sort(key = lambda x: x[0]) #t를 기준으로 정렬함
 for i in range(T):
     if inf[lst[i][1]] == 1 and cnt[lst[i][1]] < K: #감염됐고 K번 미만 전염시킴
         inf[lst[i][2]] = 1
-        cnt[lst[i][1]] += 1
     elif inf[lst[i][2]] == 1 and cnt[lst[i][2]] < K:
         inf[lst[i][1]] = 1
-        cnt[lst[i][2]] += 1
+    cnt[lst[i][1]] += 1
+    cnt[lst[i][2]] += 1
 
 for i in range(1, N+1):
     print(inf[i], end = "")

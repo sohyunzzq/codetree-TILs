b6938ce211de60ebf112bb36518443a2ecdc0625
jelly <sohyunzzq@naver.com>
2024-08-01 16:40:57 +0900

    Title: 되돌아오기 2, Time: 173ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240801/되돌아오기 2/come-back-2.py
@@ -0,0 +1,25 @@
+dx, dy = [0, 1, 0, -1], [1, 0, -1, 0]
+
+cmd = input()
+
+x, y = 0, 0
+dir_num = 0
+result = -1
+time = 0
+
+for i in range(len(cmd)):
+    time += 1
+    if cmd[i] == "F":
+        x += dx[dir_num]
+        y += dy[dir_num]
+
+        if x == 0 and y == 0:
+            result = time
+            break
+
+    elif cmd[i] == "L":
+        dir_num = (dir_num - 1) % 4
+    else:
+        dir_num = (dir_num + 1) % 4
+    
+print(result)

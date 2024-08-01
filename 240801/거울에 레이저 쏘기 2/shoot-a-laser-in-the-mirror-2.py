b9d49f0d5883c97d42886b15952239ff9a41f366
jelly <sohyunzzq@naver.com>
2024-08-01 18:30:18 +0900

    Title: 거울에 레이저 쏘기 2, Time: 349ms, Memory: 73MB, Status: Passed - CodeTree

---
+++ 240801/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
@@ -0,0 +1,56 @@
+def in_range(x, y):
+    if 0 <= x < n and 0 <= y < n:
+        return True
+    return False
+
+dict1 = {0: 1, 1: 0, 2: 3, 3: 2}
+dx, dy = [-1, 0, 1, 0], [0, 1, 0, -1]
+
+n = int(input())
+
+area = []
+for i in range(n):
+    area.append([0] * n)
+
+for i in range(n):
+    temp = input()
+    for j in range(n):
+        area[i][j] = temp[j]
+
+k = int(input())
+if 1 <= k <= 1 * n:
+    dir_num = 2
+    x = 0
+    y = k - 1
+elif 1 * n < k <= 2 * n:
+    dir_num = 3
+    x = k % n - 1
+    y = n - 1
+elif 2 * n < k <= 3 * n:
+    dir_num = 0
+    x = n - 1
+    y = n - k % n
+else:
+    dir_num = 1
+    x = n - k % n
+    y = 0
+
+cnt = 0
+while True:
+    if not in_range(x, y):
+        print(cnt)
+        break
+
+    if area[x][y] == "/":
+        dir_num = dict1[dir_num]
+    else:
+        dir_num = 3 - dir_num
+
+    x += dx[dir_num]
+    y += dy[dir_num]
+    cnt += 1
+
+'''
+/ 0: 1, 1: 0, 2: 3, 3: 2
+\ 0: 3, 1: 2, 2: 1, 3: 0
+'''

d903a311411f4961f8edf51a1a32d72ae5e3764f
jelly <sohyunzzq@naver.com>
2024-08-02 01:21:57 +0900

    Title: 원 모양으로 되어있는 방, Time: 142ms, Memory: 25MB, Status: Passed - CodeTree

--- 240802/원 모양으로 되어있는 방/a-room-in-a-circle.py
+++ 240802/원 모양으로 되어있는 방/a-room-in-a-circle.py
@@ -10,16 +10,8 @@ for i in range(n):
 
 for i in range(n):
     result = 0
-    temp = lst[i]
-    lst[i] = 0
-
     for j in range(n):
-        val = j - i
-        if val < 0:
-            val += n
-        result += val * lst[j]
+        result += (j - i + n) % n * lst[j]
     
     min_val = min(min_val, result)
-    lst[i] = temp
-
 print(min_val)

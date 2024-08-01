95cdbaa24d01b9738d03452800af957ceec508d8
jelly <sohyunzzq@naver.com>
2024-08-02 02:36:33 +0900

    Title: Carry 피하기 2, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 240802/Carry 피하기 2/escaping-carry-2.py
+++ 240802/Carry 피하기 2/escaping-carry-2.py
@@ -10,17 +10,12 @@ for i in range(n):
     for j in range(i+1, n):
         for k in range(j+1, n):
             if lst[i] % 10 + lst[j] % 10 + lst[k] % 10 >= 10:
-                #print(10, lst[i], lst[j], lst[k])
                 continue
             if lst[i] % 100 + lst[j] % 100 + lst[k] % 100 >= 100:
-               # print(100, lst[i], lst[j], lst[k])
                 continue
             if lst[i] % 1000 + lst[j] % 1000 + lst[k] % 1000 >= 1000:
-                #print(1000, lst[i], lst[j], lst[k])
                 continue
             if lst[i] % 10000 + lst[j] % 10000 + lst[k] % 10000 >= 10000:
-                #print(1000, lst[i], lst[j], lst[k])
                 continue
             result = max(result, lst[i] + lst[j] + lst[k])
-            #print(lst[i] + lst[j] + lst[k])
 print(result)

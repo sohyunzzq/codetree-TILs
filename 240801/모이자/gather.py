a72b031ed4ad73c6533e71a95df15b0eec9ed778
jelly <sohyunzzq@naver.com>
2024-08-01 20:18:01 +0900

    Title: 모이자, Time: 192ms, Memory: 25MB, Status: Passed - CodeTree

--- 240801/모이자/gather.py
+++ 240801/모이자/gather.py
@@ -6,13 +6,10 @@ lst = list(map(int, input().split()))
 min_val = sys.maxsize
 for i in range(n):
     result = 0
-    temp = lst[i]
-    lst[i] = 0
 
     for j in range(n):
         result += lst[j] * abs(i-j)
     
     min_val = min(min_val, result)
-    lst[i] = temp
 
 print(min_val)

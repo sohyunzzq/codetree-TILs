ab2dcf3140c849fe0d00d6ddcb30b2000e53d730
jelly <sohyunzzq@naver.com>
2024-08-02 02:52:35 +0900

    Title: 오목, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 240802/오목/O-mok.py
+++ 240802/오목/O-mok.py
@@ -3,19 +3,19 @@ for i in range(19):
     area.append(list(map(int, input().split())))
 
 ans = -1
-for i in range(19-4):
+for i in range(19):
     if ans != -1:
         break
-    for j in range(19-4):
-        if area[i][j] != 0 and area[i][j] == area[i][j+1] == area[i][j+2] == area[i][j+3] == area[i][j+4]:
+    for j in range(19):
+        if j < 19 - 4 and area[i][j] != 0 and area[i][j] == area[i][j+1] == area[i][j+2] == area[i][j+3] == area[i][j+4]:
             ans = area[i][j]
             x, y = i, j+2
             break
-        if area[i][j] != 0 and area[i][j] == area[i+1][j] == area[i+2][j] == area[i+3][j] == area[i+4][j]:
+        if i < 19 - 4 and area[i][j] != 0 and area[i][j] == area[i+1][j] == area[i+2][j] == area[i+3][j] == area[i+4][j]:
             ans = area[i][j]
             x, y = i+2, j
             break
-        if area[i][j] != 0 and area[i][j] == area[i+1][j+1] == area[i+2][j+2] == area[i+3][j+3] == area[i+4][j+4]:
+        if i < 19 - 4 and j < 19 - 4 and area[i][j] != 0 and area[i][j] == area[i+1][j+1] == area[i+2][j+2] == area[i+3][j+3] == area[i+4][j+4]:
             ans = area[i][j]
             x, y = i+2, j+2
             break

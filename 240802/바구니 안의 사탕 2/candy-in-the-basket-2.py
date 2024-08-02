ee5ebbe348752b563bce3476ee5e588e75c074a3
jelly <sohyunzzq@naver.com>
2024-08-02 19:05:02 +0900

    Title: 바구니 안의 사탕 2, Time: 121ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240802/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 240802/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -3,13 +3,13 @@ n, k = map(int, input().split())
 basket = [0] * 101
 for i in range(n):
     c, b = map(int, input().split())
-    basket[b] = c
+    basket[b] += c
 
 max_val = 0
-for i in range(n):
+for i in range(101-2*k):
     sum1 = 0
     for j in range(i, i + 2*k + 1):
         sum1 += basket[j]
-    
+
     max_val = max(max_val, sum1)
 print(max_val)

077f126ffe92b8dc9667130d2f01cb607af399a6
jelly <sohyunzzq@naver.com>
2024-08-02 18:00:07 +0900

    Title: 아름다운 수열 2, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240802/아름다운 수열 2/beautiful-sequence-2.py
+++ 240802/아름다운 수열 2/beautiful-sequence-2.py
@@ -2,15 +2,28 @@ n, m = map(int, input().split())
 lst1 = list(map(int, input().split()))
 lst2 = list(map(int, input().split()))
 
+dict1 = {}
+for i in lst2:
+    dict1[i] = 1
+
 cnt = 0
+
 for i in range(n-m+1):
     for j in range(m):
-        #00 01 02 / 10 11 12 / 20 21 22 / 
-        # 0  1  2 /  1  2  3 /  2  3  4
-        if lst1[i+j] not in lst2:
+        if lst1[i+j] in dict1 and dict1[lst1[i+j]] != 0:
+            dict1[lst1[i+j]] = 0
+        else:
+            for k in lst2:
+                dict1[k] = 1
             break
-        
-        if j == m-1:
-            cnt += 1
+    
+    sum1 = 0
+    for item in dict1:
+        sum1 += dict1[item]
+    if sum1 == 0:
+        cnt += 1
+
+    for j in lst2:
+        dict1[j] = 1
 
 print(cnt)

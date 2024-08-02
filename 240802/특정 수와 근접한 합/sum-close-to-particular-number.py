bc198307ffbc56553a5d4fe7da47c7144743b1d2
jelly <sohyunzzq@naver.com>
2024-08-02 12:50:13 +0900

    Title: 특정 수와 근접한 합, Time: 135ms, Memory: 25MB, Status: Passed - CodeTree

--- 240802/특정 수와 근접한 합/sum-close-to-particular-number.py
+++ 240802/특정 수와 근접한 합/sum-close-to-particular-number.py
@@ -7,10 +7,5 @@ min_val = sys.maxsize
 
 for i in range(n):
     for j in range(i+1, n):
-        temp1 = lst[i]
-        temp2 = lst[j]
-        lst[i], lst[j] = 0, 0
-        min_val = min(min_val, abs(s - sum(lst)))
-        lst[i] = temp1
-        lst[j] = temp2
+        min_val = min(min_val, abs(s - (sum(lst) - lst[i] - lst[j])))
 print(min_val)

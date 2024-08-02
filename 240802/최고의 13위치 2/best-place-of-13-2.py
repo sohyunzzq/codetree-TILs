3dff16a7bf688be100569af765f6664b091b1774
jelly <sohyunzzq@naver.com>
2024-08-02 12:24:38 +0900

    Title: 최고의 13위치 2, Time: 127ms, Memory: 25MB, Status: Passed - CodeTree

--- 240802/최고의 13위치 2/best-place-of-13-2.py
+++ 240802/최고의 13위치 2/best-place-of-13-2.py
@@ -8,8 +8,9 @@ max_val = 0
 
 for i in range(n):
     for j in range(n-2):
-        for k in range(i+1, n):
+        for k in range(n):
             for m in range(n-2):
-                max_val = max(max_val, area[i][j] + area[i][j+1] + area[i][j+2] + area[k][m] + area[k][m+1] + area[k][m+2])
+                if i != k or (i == k and j+2 < m):
+                    max_val = max(max_val, area[i][j] + area[i][j+1] + area[i][j+2] + area[k][m] + area[k][m+1] + area[k][m+2])
 
 print(max_val)

491d51439e0cba794168369df64c6452fafadb0e
jelly <sohyunzzq@naver.com>
2024-08-02 18:13:31 +0900

    Title: 아름다운 수열 2, Time: 118ms, Memory: 25MB, Status: Passed - CodeTree

--- 240802/아름다운 수열 2/beautiful-sequence-2.py
+++ 240802/아름다운 수열 2/beautiful-sequence-2.py
@@ -2,28 +2,26 @@ n, m = map(int, input().split())
 lst1 = list(map(int, input().split()))
 lst2 = list(map(int, input().split()))
 
-dict1 = {}
+check = []
+
 for i in lst2:
-    dict1[i] = 1
+    check.append([i, 1])
 
 cnt = 0
 
 for i in range(n-m+1):
     for j in range(m):
-        if lst1[i+j] in dict1 and dict1[lst1[i+j]] != 0:
-            dict1[lst1[i+j]] = 0
-        else:
-            for k in lst2:
-                dict1[k] = 1
-            break
-    
+        for k in range(m):
+            if lst1[i+j] == check[k][0] and check[k][1] != 0:
+                check[k][1] = 0
+                break
     sum1 = 0
-    for item in dict1:
-        sum1 += dict1[item]
+    for j in check:
+        sum1 += j[1]
     if sum1 == 0:
         cnt += 1
 
-    for j in lst2:
-        dict1[j] = 1
+    for q in check:
+        q[1] = 1
 
 print(cnt)

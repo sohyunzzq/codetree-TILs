e437e65cc2a0f210e63c1fb0ad2013f318cd9467
jelly <sohyunzzq@naver.com>
2024-08-02 18:19:01 +0900

    Title: 아름다운 수열 2, Time: 195ms, Memory: 24MB, Status: Passed - CodeTree

--- 240802/아름다운 수열 2/beautiful-sequence-2.py
+++ 240802/아름다운 수열 2/beautiful-sequence-2.py
@@ -2,26 +2,10 @@ n, m = map(int, input().split())
 lst1 = list(map(int, input().split()))
 lst2 = list(map(int, input().split()))
 
-check = []
-
-for i in lst2:
-    check.append([i, 1])
-
 cnt = 0
 
-for i in range(n-m+1):
-    for j in range(m):
-        for k in range(m):
-            if lst1[i+j] == check[k][0] and check[k][1] != 0:
-                check[k][1] = 0
-                break
-    sum1 = 0
-    for j in check:
-        sum1 += j[1]
-    if sum1 == 0:
+for i in range(n):
+    if sorted(lst1[i:i+m]) == sorted(lst2):
         cnt += 1
-
-    for q in check:
-        q[1] = 1
 
 print(cnt)

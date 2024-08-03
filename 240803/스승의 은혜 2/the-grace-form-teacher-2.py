96e073c0655a76afdd1812f2cf884fbd0afcd475
jelly <sohyunzzq@naver.com>
2024-08-03 23:47:51 +0900

    Title: 스승의 은혜 2, Time: 132ms, Memory: 25MB, Status: Passed - CodeTree

--- 240803/스승의 은혜 2/the-grace-form-teacher-2.py
+++ 240803/스승의 은혜 2/the-grace-form-teacher-2.py
@@ -3,17 +3,18 @@ n, b = map(int, input().split())
 lst = []
 for i in range(n):
     lst.append(int(input()))
+lst.sort()
 
 max_val = 0
 for i in range(n):
-    temp = lst[i] // 2
-    cnt = 1
+    cnt = 0
+    temp = 0
+    lst[i] //= 2
     for j in range(n):
-        if i == j:
-            continue
         temp += lst[j]
         cnt += 1
         if temp <= b:
             max_val = max(max_val, cnt)
+    lst[i] *= 2
 
 print(max_val)

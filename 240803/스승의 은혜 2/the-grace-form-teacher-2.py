e1e7aa4e6e5ef88c5da6467c55a913a704d48f42
jelly <sohyunzzq@naver.com>
2024-08-03 18:20:08 +0900

    Title: 스승의 은혜 2, Time: 116ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240803/스승의 은혜 2/the-grace-form-teacher-2.py
+++ 240803/스승의 은혜 2/the-grace-form-teacher-2.py
@@ -5,13 +5,15 @@ for i in range(n):
     lst.append(int(input()))
 
 max_val = 0
-
 for i in range(n):
-    temp = lst[i] / 2
+    temp = lst[i] // 2
+    cnt = 1
     for j in range(n):
         if i == j:
             continue
         temp += lst[j]
+        cnt += 1
         if temp <= b:
-            max_val = max(max_val, j+1)
+            max_val = max(max_val, cnt)
+
 print(max_val)

1d4db02f7f9f07cfac611050753966fd5efb2d4a
jelly <sohyunzzq@naver.com>
2024-08-03 18:02:59 +0900

    Title: 겹치지 않는 선분 2, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240803/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
+++ 240803/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
@@ -13,8 +13,21 @@ for i in range(n):
     x1, x2 = lst[i][0], lst[i][1]
     for j in range(i+1, n):
         x3, x4 = lst[j][0], lst[j][1]
-        if x1 <= x3 <= x2 and x1 <= x4 <= x2:
-            check[i] = 0
-            check[j] = 0
+        if x1 <= x2 and x3 <= x4: #// 방향
+            if x1 <= x3 <= x2 and x1 <= x4 <= x2:
+                check[i] = 0
+                check[j] = 0
+        elif x1 >= x2 and x3 >= x4: #\\ 방향
+            if x2 <= x3 <= x1 and x2 <= x4 <= x1:
+                check[i] = 0
+                check[j] = 0
+        elif x1 <= x2 and x3 >= x4: #/\
+            if x1 <= x4 <= x2:
+                check[i] = 0
+                check[j] = 0
+        else: #\/
+            if x2 <= x3 <= x1:
+                check[i] = 0
+                check[j] = 0
 
 print(sum(check))

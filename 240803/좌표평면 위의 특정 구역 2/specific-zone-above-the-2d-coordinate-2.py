6fd75a65304c924e25beee7cf32f7f498c1f8496
jelly <sohyunzzq@naver.com>
2024-08-03 14:56:59 +0900

    Title: 좌표평면 위의 특정 구역 2, Time: 160ms, Memory: 25MB, Status: Passed - CodeTree

--- 240803/좌표평면 위의 특정 구역 2/specific-zone-above-the-2d-coordinate-2.py
+++ 240803/좌표평면 위의 특정 구역 2/specific-zone-above-the-2d-coordinate-2.py
@@ -1,5 +1,4 @@
 import sys
-MAX_COOR = 400
 
 n = int(input())
 
@@ -10,24 +9,23 @@ for i in range(n):
 ans = sys.maxsize	
 
 for i in range(n):
-    area = []
-    for j in range(MAX_COOR + 1):
-        area.append([0] * (MAX_COOR + 1))
-    
     for j in range(n): #순회
         if i == j:
             continue
-        area[lst[j][0]][lst[j][1]] = 1
     
     minx, miny = sys.maxsize, sys.maxsize
     maxx, maxy = 0, 0
-    for x in range(1, MAX_COOR + 1):
-        for y in range(1, MAX_COOR + 1):
-            if area[x][y] == 1:
-                minx = min(minx, x)
-                miny = min(miny, y)
-                maxx = max(maxx, x)
-                maxy = max(maxy, y)
+
+    for j in range(n):
+        if i == j:
+            continue
+        x = lst[j][0]
+        y = lst[j][1]
+
+        minx = min(minx, x)
+        miny = min(miny, y)
+        maxx = max(maxx, x)
+        maxy = max(maxy, y)
     
     ans = min((maxx - minx) * (maxy - miny), ans)
 

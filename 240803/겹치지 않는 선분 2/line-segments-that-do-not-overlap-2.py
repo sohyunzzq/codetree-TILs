ec22e97e52f7d9811385eb28a2e641b613b787ff
jelly <sohyunzzq@naver.com>
2024-08-03 18:07:14 +0900

    Title: 겹치지 않는 선분 2, Time: 139ms, Memory: 25MB, Status: Passed - CodeTree

--- 240803/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
+++ 240803/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
@@ -3,7 +3,7 @@ n = int(input())
 lst = []
 for i in range(n):
     lst.append(list(map(int, input().split())))
-lst.sort(key = lambda x: x[0])
+lst.sort(key = lambda x: min(x[0], x[1]))
 
 #언제 겹치는가?
 #다른 선분의 두 좌표가 모두 다른 선분의 두 좌표 사이에 있을 때
@@ -25,7 +25,7 @@ for i in range(n):
             if x1 <= x4 <= x2 or x1 <= x3 <= x2:
                 check[i] = 0
                 check[j] = 0
-        else: #\/
+        elif x1 >= x2 and x3 <= x4:
             if x2 <= x3 <= x1 or x2 <= x4 <= x1:
                 check[i] = 0
                 check[j] = 0

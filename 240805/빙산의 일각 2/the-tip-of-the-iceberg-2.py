4a5057dc492c8f13d8ab797e5f1819b3ee749155
jelly <sohyunzzq@naver.com>
2024-08-05 03:29:43 +0900

    Title: 빙산의 일각 2, Time: 132ms, Memory: 25MB, Status: Passed - CodeTree

--- 240805/빙산의 일각 2/the-tip-of-the-iceberg-2.py
+++ 240805/빙산의 일각 2/the-tip-of-the-iceberg-2.py
@@ -9,27 +9,23 @@ iceberg = []
 for i in range(n):
     iceberg.append(int(input()))
 
+# 010000100
 # 1101011001 이런 식으로 있다고 쳤을 때
-# 1 뭉텅이가 몇 개인지 세야 하는데
-# 1번 인덱스부터 시작해서
-# 0에서 1이 되면 cnt+1
-# 0, 1번 중 하나라도 1이면 +1
+# 1 뭉텅이가 몇 개인지 세야 함
 
 ans = 0
 for i in range(max(iceberg)):
-    if n == 1:
-        ans = 1
-        break
+    check = [0] * n
+    for j in range(n):
+        if iceberg[j] > i:
+            check[j] = 1
+
     cnt = 0
-    past = up(iceberg[0], i)
-    if past + up(iceberg[1], i) != 0:
-        cnt += 1
-    for j in range(1, n):
-        now = up(iceberg[j], i)
-        if past == 0 and now == 1:
+    p = 0
+    for j in range(n):
+        if check[j] == 1 and p == 0:
             cnt += 1
-        past = now
-    
+        p = check[j]
     ans = max(ans, cnt)
 
 print(ans)

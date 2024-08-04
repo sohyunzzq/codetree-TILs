ab329303e3b712a3b60340dbf77d88204049e2e1
jelly <sohyunzzq@naver.com>
2024-08-04 23:55:37 +0900

    Title: 흥미로운 숫자 2, Time: 124ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240804/흥미로운 숫자 2/interesting-numbers-2.py
@@ -0,0 +1,22 @@
+x, y = map(int, input().split())
+
+ans = 0
+for num in range(x, y+1):
+    num = str(num)
+    check = [0] * 10
+
+    for i in range(len(num)):
+        check[int(num[i])] += 1
+    
+    same = 0 #2개 이상 같은 숫자
+    diff = 0 #하나만 다른 숫자
+    for i in range(10):
+        if check[i] == 1:
+            same += 1
+        elif check[i] >= 2:
+            diff += 1
+    
+    if same + diff == 2:
+        ans += 1
+
+print(ans)

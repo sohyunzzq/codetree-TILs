60dec2e48b2b601470e6432e5124a148ffd557ed
jelly <sohyunzzq@naver.com>
2024-08-05 16:26:31 +0900

    Title: 수를 여러번 사용하여 특정 수 만들기, Time: 133ms, Memory: 24MB, Status: Passed - CodeTree

--- 240805/수를 여러번 사용하여 특정 수 만들기/create-a-specific-number-using-multiple-numbers.py
+++ 240805/수를 여러번 사용하여 특정 수 만들기/create-a-specific-number-using-multiple-numbers.py
@@ -1,22 +1,15 @@
 a, b, c = map(int, input().split())
 
 ans = 0
-anum = c // a
-bnum = 0
     
-for i in range(anum + 1):
-    tmp = (anum - i) * a
+for i in range(c // a + 1):
+    tmp = a * i #더 작은 수인 a를 i번 더하기
+    
+    #이제 목표는, tmp에 b를 더해서 c에 가까워지는 것
+    #tmp에 최대 얼마만큼 b를 넣을 수 있는지, a로 계산했던 것처럼 계산
+    #a는 0에 더하는 거였지만 여기는 이미 tmp가 존재하므로 계산 주의
 
-    while tmp + b <= c:
-        tmp += b
-
+    tmp += b * ((c - tmp) // b)
     ans = max(ans, tmp)
 
 print(ans)
-    
-'''
-26 783 882
-
-26*3 + 783
-
-'''

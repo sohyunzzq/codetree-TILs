21680c7f501716a9b36b47c290be5eec30d1581c
jelly <sohyunzzq@naver.com>
2024-08-06 03:39:12 +0900

    Title: 독서실의 거리두기 5, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240806/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
+++ 240806/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
@@ -13,21 +13,15 @@ for i in range(n):
     number = sum(seat)
     #자리를 배치했고, 이 배치에서 최소거리를 구함
 
-    index = 0
     min_dis = n
 
     for j in range(n):
-        if seat[j] == 1 and index < number - 1: #여기서부터 다음 1까지의 거리를 구함
-            dis = 0
-            index += 1
-            
-            for k in range(j + 1, n):
-                if seat[k] == 1:
-                    dis += 1
-                    break
-                dis += 1
-            
-        min_dis = min(min_dis, dis)
+        for k in range(j+1, n):
+            if seat[j] == 1 and seat[k] == 1: #여기서부터 다음 1까지의 거리를 구함
+                dis = abs(k-j)
+            #dis 구해짐
+        
+            min_dis = min(min_dis, dis)
     
     ans = max(ans, min_dis)
 

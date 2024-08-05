f14402af6452f2880af42fb611348a5921e5a37f
jelly <sohyunzzq@naver.com>
2024-08-05 21:02:54 +0900

    Title: 3개의 선 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240805/3개의 선 2/three-lines-2.py
+++ 240805/3개의 선 2/three-lines-2.py
@@ -1,91 +1,61 @@
-'''
-1. 2차원 좌표 평면이 있다.
-2. 서로 다른 N개의 점이 주어진다.
-3. x축 혹은 y축에 평행한 직선 3개를 만든다.
-4. 주어진 모든 점들을 지날 수 있는지 확인한다.
+n = int(input())
 
-1- 내 풀이
+coor = []
+for i in range(n):
+    coor.append(list(map(int, input().split())))
 
-1) 좌표 평면을 나타낼 2차원 배열 arr를 만든다. (점의 위치가 0이상 10이하 이기에 11x11)
-2) for nc in range(N) 순회하며 점을 좌표평면에 그린다.
-3) for i in range(11)를 순회하며 1번 선을 정한다.
-4) for j in range(11)를 순회하며 2번 선을 정한다.
-5) for k in range(11)를 순회하며 3번 선을 정한다.
-6) for check in range(4)를 순회하며 1 2 3번이 차례로 x x x, x x y, x y y, y y y 
-4가지 경우의 수를 모두 체크한다.
-7) 임시로 선을 그어볼 temp_arr 배열도 만들어 둔 뒤 5번 반복문이 새로 시작될 때 마다 갱신되게 한다.
-8) 선을 그릴 때는 해당 라인의 점이 != 0 일 경우 temp_arr의 해당 점을 0으로 만들고 만난 점 숫자를 count+=1한다.
-9) count == N이라면 맞다면 1을 출력하고 프로그램을 종료한다.
-10) 만약 프로그램이 종료되지 않고 2번 반복문까지 돌았을 경우에는 0을 출력한다.
-'''
+#가능한 경우 (x/y에 평행한 선분 개수)
+#x3 y0 
+#x2 y1
+#x1 y2
+#x0 y3
 
+#000, 001, ..., 101010 순회를 돌 거임
+#x=0, x=0, x=0 이 경우 상관없음. 중복 안 된다는 말 없음
+#000을 가지고 xxx, xxy, xyy, yyy 전부 다 검사를 할 거임
+#000 - xxx - 좌표 이렇게 들어가서, 모든 좌표가 통과되면 result는 1
 
-def new_temp_arr():
-    new_temp = [[0]*11 for _ in range(11)]
+result = 0
 
-    for i in range(11):
-        for j in range(11):
-            new_temp[i][j] = arr[i][j]
+for i in range(11):
+    for j in range(11):
+        for k in range(11):
+            check = True
+            for x, y in coor:
+                if x == i or x == j or x == k: #모두 x에 걸리냐
+                    continue
+                check = False
+            
+            if check:
+                ans = 1
+                break
 
-    return new_temp
+            check = True
+            for x, y in coor:
+                if x == i or x == j or y == k:
+                    continue
+                check = False
+            
+            if check:
+                ans = 1
+                break
 
+            for x, y in coor:
+                if x == i or y == j or y == k:
+                    continue
+                check = False
 
-def check_x(x):
+            if check:
+                ans = 1
+                break
+            
+            for x, y in coor:
+                if y == i or y == j or y == k:
+                    continue
+                check = False
+            
+            if check:
+                ans = 1
+                break
 
-    count = 0
-    for i in range(11):
-        if temp_arr[i][x] != 0:
-            temp_arr[i][x] = 0
-            count += 1
-
-    return count
-
-
-def check_y(y):
-
-    count = 0
-    for i in range(11):
-        if temp_arr[y][i] != 0:
-            temp_arr[y][i] = 0
-            count += 1
-
-    return count
-
-
-N = int(input())
-
-arr = [[0]*11 for _ in range(11)]
-
-for i in range(N):
-    x, y = map(int, input().split())
-    arr[y][x] = 1
-
-for l1 in range(11):
-    for l2 in range(11):
-        for l3 in range(11):
-            for check in range(4):
-                temp_arr = new_temp_arr()
-
-                result = 0
-                if check == 0:
-                    result += check_x(l1)
-                    result += check_x(l2)
-                    result += check_x(l3)
-                if check == 1:
-                    result += check_x(l1)
-                    result += check_x(l2)
-                    result += check_y(l3)
-                if check == 2:
-                    result += check_x(l1)
-                    result += check_y(l2)
-                    result += check_y(l3)
-                if check == 3:
-                    result += check_y(l1)
-                    result += check_y(l2)
-                    result += check_y(l3)
-                
-                if result == N:
-                    print(1)
-                    exit()
-
-print(0)
+print(ans)

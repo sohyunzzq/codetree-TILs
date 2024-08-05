1c235df6a493431f8fd2abe96cc7c7110ae414e9
jelly <sohyunzzq@naver.com>
2024-08-06 01:38:53 +0900

    Title: 독서실의 거리두기 5, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240806/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
+++ 240806/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
@@ -1,24 +1,34 @@
 n = int(input())
 seat = list(input())
+for i in range(n):
+    seat[i] = int(seat[i])
 
 ans = 0
 for i in range(n):
-    if seat[i] == "0":
-        seat[i] = "1"
+    if seat[i] == 0:
+        seat[i] = 1
     else:
         continue
-    print(seat)
-    tmp = n
-    for j in range(n): #거리 세기
-        if seat[j] == "1":
-            dis = 1
-            for k in range(j+1, n):
-                if seat[k] == "1" or k == n-1:
+
+    number = sum(seat)
+    #자리를 배치했고, 이 배치에서 최소거리를 구함
+
+    index = 0
+    min_dis = n
+
+    for j in range(n):
+        if seat[j] == 1 and index < number - 1: #여기서부터 다음 1까지의 거리를 구함
+            dis = 0
+            index += 1
+            
+            for k in range(j + 1, n):
+                if seat[k] == 1:
+                    dis += 1
                     break
                 dis += 1
-            if seat[n-1] == "0" and k != n-1:
-                tmp = min(tmp, dis)
-    print(tmp)
-    ans = max(ans, tmp)
-    seat[i] = "0"
+            
+            min_dis = min(min_dis, dis)
+            
+        ans = max(ans, min_dis)
+    seat[i] = 0
 print(ans)

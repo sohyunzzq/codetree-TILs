7b17420955f37061225d9bca421a805cdba6e071
jelly <sohyunzzq@naver.com>
2024-08-06 03:41:19 +0900

    Title: 독서실의 거리두기 5, Time: 107ms, Memory: 25MB, Status: Passed - CodeTree

--- 240806/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
+++ 240806/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
@@ -18,12 +18,9 @@ for i in range(n):
     for j in range(n):
         for k in range(j+1, n):
             if seat[j] == 1 and seat[k] == 1: #여기서부터 다음 1까지의 거리를 구함
-                dis = abs(k-j)
-            #dis 구해짐
-        
-            min_dis = min(min_dis, dis)
-    
-    ans = max(ans, min_dis)
+                min_dis = min(min_dis, abs(k-j))
 
+    ans = max(ans, min_dis)
     seat[i] = 0
+    
 print(ans)

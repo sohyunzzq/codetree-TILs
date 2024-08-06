ab0090d4dbf2ba36afd4843e71716c1bdeccd2f4
jelly <sohyunzzq@naver.com>
2024-08-07 00:52:15 +0900

    Title: 가장 많이 나온 쌍, Time: 126ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240807/가장 많이 나온 쌍/the-most-frequent-pair.py
@@ -0,0 +1,18 @@
+n, m = map(int, input().split())
+
+lst = []
+for i in range(m):
+    lst.append(list(map(int, input().split())))
+
+ans = 0
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        if i == j:
+            continue
+        cnt = 0
+        for k in range(m):
+            if (i == lst[k][0] and j == lst[k][1]) or (i == lst[k][1] and j == lst[k][0]):
+                cnt += 1
+        ans = max(cnt, ans)
+
+print(ans)

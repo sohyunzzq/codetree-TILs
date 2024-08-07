a507826538fd3be430f8bea155fbab02a306e1c9
jelly <sohyunzzq@naver.com>
2024-08-08 03:19:15 +0900

    Title: 훌륭한 점프, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240808/훌륭한 점프/great-jump.py
+++ 240808/훌륭한 점프/great-jump.py
@@ -1,9 +1,9 @@
-def jump(i):
+def jump(x):
     tmp = [0]
     for index, item in enumerate(lst):
-        if item <= i:
-            tmp.append(index)
-
+        if index <= x:
+            tmp.append(item)
+    tmp.append(n-1)
     for i in range(len(tmp) - 1):
         if tmp[i+1] - tmp[i] > k:
             return False
@@ -16,13 +16,9 @@ lst = list(map(int, input().split()))
 #최댓값이 최소가 되도록
 #2 3 5 4 1
 
-#5부터 내려가면서 답이라고 가정함
-#5보다 작은 것들을 새로운 리스트에 담고, 그것들이 다 k 이내인지 검사
-
-ans = n
-
-for i in range(n, 0, -1): #i가 정답이라고 가정
+temp = set(sorted(lst, reverse = True))
+ans = max(lst)
+for i in temp:
     if jump(i):
         ans = min(ans, i)
-
 print(ans)

27d9a9ff6845d6883283b9451aed08bf980de0be
jelly <sohyunzzq@naver.com>
2024-08-07 23:22:46 +0900

    Title: 숫자들의 최대 차, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240807/숫자들의 최대 차/maximum-difference-in-numbers.py
@@ -0,0 +1,24 @@
+n, k = map(int, input().split())
+
+lst = []
+for i in range(n):
+    lst.append(int(input()))
+
+#정렬을 하고, 최소 고정해놓고 최대에서 하나씩 줄여가면서 차이 넘는지 체크하기
+#1 1 3 4 6
+#1 1 3 4 6 7 8 11 15 일 때 개수 세는 법
+#3부터 11까지 센다 → 6개임
+#인덱스 2, 7이므로 max_val - min_val + 1
+
+lst.sort()
+
+ans = 0
+for i in range(n): #최솟값 고정
+    min_val = lst[i]
+    for j in range(n-1, i, -1): #뒤에서부터 고정해둔 최솟값까지 접근
+        max_val = lst[j]
+        if max_val > min_val + k:
+            continue
+        ans = max(ans, max_val - min_val + 1)
+
+print(ans)

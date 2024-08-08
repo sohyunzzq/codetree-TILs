d8e8a8cfddd1c943b40851a2b61474c9bae4958c
jelly <sohyunzzq@naver.com>
2024-08-09 02:47:54 +0900

    Title: 최대 H 점수 2, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240809/최대 H 점수 2/maximum-h-score-2.py
@@ -0,0 +1,28 @@
+n, l = map(int, input().split())
+lst = list(map(int, input().split()))
+
+#수열에서 h 이상인 숫자가 h개 이상인가?를 찾아야 됨
+#h개 이상이어야 하므로 될 수 있는 후보는 0 ~ n
+#n개 중 l개는 1을 더해야 함
+#h숫자가 0일 때부터 따져보기
+#리스트를 정렬?
+#100 3 2 1
+
+lst.sort(reverse = True)
+
+ans = 0
+for i in range(n):
+    cnt = 0
+    down = l
+    for j in range(n):
+        if lst[j] >= i:
+            cnt += 1
+            continue
+        if down == 0:
+            break
+        elif lst[j] + 1 >= i:
+            cnt += 1
+            down -= 1
+    if cnt >= i:
+        ans = max(ans, i)
+print(ans)

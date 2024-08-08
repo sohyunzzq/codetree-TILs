524494998964623bc03a954347935d9c724ed906
jelly <sohyunzzq@naver.com>
2024-08-09 02:55:30 +0900

    Title: 최대 최소간의 차, Time: 121ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240809/최대 최소간의 차/difference-between-maximum-and-minimum.py
@@ -0,0 +1,21 @@
+import sys
+
+n, k = map(int, input().split())
+lst = list(map(int, input().split()))
+
+#[i, i+k]
+#i보다 작으면 i까지 올려주고
+#i+k보다 크면 i+k까지 낮춰줌
+
+ans = sys.maxsize
+for i in range(10000):
+    cost = 0
+    for j in range(n):
+        if lst[j] < i:
+            cost += i - lst[j]
+        elif lst[j] > i + k:
+            cost += lst[j] - (i + k)
+    
+    ans = min(ans, cost)
+
+print(ans)

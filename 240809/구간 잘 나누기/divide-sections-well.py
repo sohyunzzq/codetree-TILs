dc5978f520e2d928beacd433e44527ae39f079a7
jelly <sohyunzzq@naver.com>
2024-08-09 01:50:55 +0900

    Title: 구간 잘 나누기, Time: 146ms, Memory: 25MB, Status: Passed - CodeTree

--- 240809/구간 잘 나누기/divide-sections-well.py
+++ 240809/구간 잘 나누기/divide-sections-well.py
@@ -4,7 +4,7 @@ lst = list(map(int, input().split()))
 # 잘 생각해 보면, 구간의 합의 최댓값을 결정한다면
 # 구간을 몇 개로 나눠야 하는지 손쉽게 찾을 수 있습니다.
 
-for i in range(max(lst), 100*100 + 1):
+for i in range(max(lst), 100*n + 1):
     # 구간의 합의 최댓값이 i일 때
 
     # check: 구간을 나눌 수 있으면 true

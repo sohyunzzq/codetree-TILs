6d78741029147bef90d22d3a902387df3ec5220e
jelly <sohyunzzq@naver.com>
2024-08-09 22:56:19 +0900

    Title: 연속된 숫자 만들기 2, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 240809/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
+++ 240809/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
@@ -1,24 +1,8 @@
 a, b, c = map(int, input().split())
 
-#맨 끝 사람이 나머지 사이로
-#2 6 9
-#6 8 9
-#6 7 8
-
-#2 6 9
-#6 7 9
-#7 8 9
-
-#1 7 12
-#7 9 12
-
-#더 먼 사람을 가까이 오게 하기
-#그런데 나머지 두 수가 연속되면 올 수 없음
-#가까이 부를 때 2만큼
-
-if (a + 1 == b and b + 1 == c) or (a - 1 == b and b - 1 == c):
+if (a + 1 == b and b + 1 == c) or (a - 1 == b and b - 1 == c): #이미 연속됨
     print(0)
-elif (a + 2 == b) or (b + 2 == c) or (a - 2 == b) or (b - 2 == c):
+elif (a + 2 == b) or (b + 2 == c) or (a - 2 == b) or (b - 2 == c): #가운데 낄 수 있음
     print(1)
 else:
     print(2)

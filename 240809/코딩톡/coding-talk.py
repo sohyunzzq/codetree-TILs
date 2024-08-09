e2a6e690ebcb2de0db816ce1019eb6b66c7501e0
jelly <sohyunzzq@naver.com>
2024-08-09 19:24:44 +0900

    Title: 코딩톡, Time: 128ms, Memory: 24MB, Status: Passed - CodeTree

--- 240809/코딩톡/coding-talk.py
+++ 240809/코딩톡/coding-talk.py
@@ -2,12 +2,13 @@ def talk():
     if int(lst[p-1][1]) == 0: #다 읽으면 아무것도 출력 X
         return
 
-    read = [0] * n
-    for i in range(p-1, m):
+    read = [0] * n #읽었으면 1
+
+    for i in range(p-1, m): #나랑 나 다음 보낸 사람들은 제외
         read[ord(lst[i][0]) - 65] = 1
     
     for i in range(p-1):
-        if int(lst[i][1]) == int(lst[p-1][1]):
+        if int(lst[i][1]) == int(lst[p-1][1]): #B 2 A 2 인 경우, B는 읽은 거
             read[ord(lst[i][0]) - 65] = 1
     
     for i in range(n):
@@ -23,16 +24,3 @@ for i in range(m):
     lst.append(list(input().split()))
 
 talk()
-
-'''
-내 위로 보낸 사람, 나 제외
-0이면 걍 바로 끝
-후보 중에서, 내가 보내기 전에 보냈고, 나랑 숫자 똑같으면 제외
-
-D 0:                [E]
-C 1: D E            [D E]
-B 2: C D E          [C D E]
-B 2: C D E         
-A 2: C D E          [B C D E] 
-F 4: A B C D E      [A B C D E]
-'''

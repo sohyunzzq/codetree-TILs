8e6bb960b0c5339e553ba94f04d1a26eba1f4a61
jelly <sohyunzzq@naver.com>
2024-08-09 20:54:02 +0900

    Title: X 달리기, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 240809/X 달리기/x-run.py
+++ 240809/X 달리기/x-run.py
@@ -6,6 +6,11 @@ time = 0
 left = 0
 right = x
 
+#양쪽에서 좁혀가기
+#만약 속도를 더 높였을 때
+#왼쪽은 갈 수 있고 오른쪽은 못 가면, 왼쪽이라도 가기
+#부딪히면 그냥 나오고, 나머지 거리를 가기 위해 +1
+
 while True:
     if left + speed < right - speed:
         left += speed
@@ -23,10 +28,3 @@ if left != right:
     time += 1
     
 print(time)
-
-
-'''
-231 280 22
-253 258
-
-'''

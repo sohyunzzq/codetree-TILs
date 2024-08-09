9bb150e1ab7c981828d8c724ae58d25cade9d46a
jelly <sohyunzzq@naver.com>
2024-08-09 19:14:29 +0900

    Title: 코딩톡, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240809/코딩톡/coding-talk.py
+++ 240809/코딩톡/coding-talk.py
@@ -1,3 +1,19 @@
+def talk():
+    if int(lst[p-1][1]) == 0: #다 읽으면 아무것도 출력 X
+        return
+
+    read = [0] * n
+    for i in range(p-1, m):
+        read[ord(lst[i][0]) - 65] = 1
+    
+    for i in range(p-1):
+        if int(lst[i][1]) == int(lst[p-1][1]):
+            read[ord(lst[i][0])] = 1
+    
+    for i in range(n):
+        if read[i] == 0:
+            print(alpha[i], end = " ")
+
 alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 n, m, p = map(int, input().split())
@@ -6,42 +22,17 @@ lst = []
 for i in range(m):
     lst.append(list(input().split()))
 
-#p번째 메세지를 읽지 않은 사람 구해라
-#내가 보내고 보낸 사람은 아님. 제외
-#나 제외
-#후보 중에, 내가 보내기 전에 보냈던 사람이랑 나랑 숫자 똑같으면 걔도 아님
-
-read = [0] * n
-
-for i in range(p-1, m): #나 + 내가 보내고 보낸 사람 1로 표시
-    read[ord(lst[i][0]) - 65] = 1
-
-for i in range(p-1): #내가 보내기 전에
-    if int(lst[i][1]) == int(lst[p-1][1]):
-        read[ord(lst[i][0]) - 65] = 1
-
-for i in range(n):
-    if read[i] == 0:
-        print(alpha[i], end = " ")
+talk()
 
 '''
-6 6 5
-D 0
-C 1
-B 2
-B 2
-A 2
-F 4
+내 위로 보낸 사람, 나 제외
+0이면 걍 바로 끝
+후보 중에서, 내가 보내기 전에 보냈고, 나랑 숫자 똑같으면 제외
 
-C 1
-A B C D E F
-x x x     x
-
-B 2
-A B C D E F
-x x       x
-
-A 2
-A B C D E F
-x         x
+D 0:                [E]
+C 1: D E            [D E]
+B 2: C D E          [C D E]
+B 2: C D E         
+A 2: C D E          [B C D E] 
+F 4: A B C D E      [A B C D E]
 '''

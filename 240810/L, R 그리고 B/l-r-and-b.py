ce915f9c1de4df2152c59390a966fe265698ac3e
jelly <sohyunzzq@naver.com>
2024-08-10 03:32:05 +0900

    Title: L, R 그리고 B, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

--- 240810/L, R 그리고 B/l-r-and-b.py
+++ 240810/L, R 그리고 B/l-r-and-b.py
@@ -13,10 +13,10 @@ for i in range(10):
 
 #L에서 R을 피해 B로
 
-ans = abs(lx - bx) + abs(ly - by) - 1
+ans = abs(lx - bx) + abs(ly - by) - 1 #ㄱ 모양이나 ㄴ 모양으로 가기
 
+#ㅣ 모양인데 가운데 가로막음 or  ㅡ 모양인데 가운데 가로막음
 if (bx == lx and rx == bx and (ly <= ry <= by or by <= ry <= ly)) or ((by == ly and ry == by) and (bx <= rx <= lx or lx <= rx <= bx)):
-    ans += 2 #ㅣ 모양인데 가운데 가로막음
-#elif #ㅡ 모양인데 가운데 가로막음
-
+    ans += 2
+    
 print(ans)

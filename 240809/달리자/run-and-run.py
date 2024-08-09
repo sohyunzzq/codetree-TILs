f327dff97886b3d5ac640917237d0620d43a3b09
jelly <sohyunzzq@naver.com>
2024-08-09 16:25:26 +0900

    Title: 달리자, Time: 135ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240809/달리자/run-and-run.py
@@ -0,0 +1,11 @@
+n = int(input())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+ans = 0
+for i in range(n-1):
+    if a[i] != b[i]:
+        ans += a[i] - b[i]
+        a[i+1] += a[i] - b[i]
+
+print(ans)

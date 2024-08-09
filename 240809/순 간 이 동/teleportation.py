a96db24dda8eb69fe050fe45e150e0e584e77c89
jelly <sohyunzzq@naver.com>
2024-08-09 22:38:04 +0900

    Title: 순 간 이 동, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

--- 240809/순 간 이 동/teleportation.py
+++ 240809/순 간 이 동/teleportation.py
@@ -4,7 +4,7 @@ a, b, x, y = map(int, input().split())
 #a - x - y - b  :3 - 2/8 - 10
 #a - y - x - b  :3 - 8/2 - 10
 
-ans1 = b - a
+ans1 = abs(b-a)
 ans2 = abs(x-a) + abs(b-y)
 ans3 = abs(y-a) + abs(b-x)
 

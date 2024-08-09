f56ab39ea5083ac1cafab9737c4e646a0c5f63cd
jelly <sohyunzzq@naver.com>
2024-08-09 23:57:09 +0900

    Title: 독서실의 거리두기 3, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240809/독서실의 거리두기 3/study-cafe-keeping-distance-3.py
+++ 240809/독서실의 거리두기 3/study-cafe-keeping-distance-3.py
@@ -7,20 +7,20 @@ for i in range(n):
 #가장 먼 두 곳 찾아서 사이에 껴주기
 
 maxdis = 0
-result = 0
 for i in range(n):
     for j in range(i+1, n):
         if seat[i] == 1 and seat[j] == 1:
-            if j-i == 1:
-                result = 1
-                break
             if maxdis < j-i:
                 maxdis = j-i
                 s1 = i
                 s2 = j
             break
+seat[(j-i)//2] = 1
 
-if result:
-    print(1)
-else:
-    print((s2-s1)//2)
+ans = n
+for i in range(n):
+    for j in range(i+1, n):
+        if seat[i] == 1 and seat[j] == 1:
+            ans = min(ans, j-i)
+            break
+print(ans)

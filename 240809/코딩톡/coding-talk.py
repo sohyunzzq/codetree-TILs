69e1389be50d7886cac4aebb66790e611fcbe91a
jelly <sohyunzzq@naver.com>
2024-08-09 17:13:00 +0900

    Title: 코딩톡, Time: 116ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240809/코딩톡/coding-talk.py
+++ 240809/코딩톡/coding-talk.py
@@ -7,15 +7,41 @@ for i in range(m):
     lst.append(list(input().split()))
 
 #p번째 메세지를 읽지 않은 사람 구해라
-#lst[p-1]번째 메세지를 확실하게 읽은 사람을 빼기
-#p-1부터 m까지 나온 알파벳들은 확실하게 읽음
-#참가자는 alpha[0] ~ alpha[n-1]
+#내가 보내고 보낸 사람은 아님. 제외
+#나 제외
+#후보 중에, 내가 보내기 전에 보냈던 사람이랑 나랑 숫자 똑같으면 걔도 아님
 
-read = [0] * n #읽으면 1로 표시
+read = [0] * n
 
-for i in range(p-1, m):
-    read[ord(lst[i][0])-65] = 1
+for i in range(p-1, m): #나 + 내가 보내고 보낸 사람 1로 표시
+    read[ord(lst[i][0]) - 65] = 1
+
+for i in range(p-1): #내가 보내기 전에
+    if int(lst[i][1]) == int(lst[p-1][1]):
+        read[ord(lst[i][0]) - 65] = 1
 
 for i in range(n):
     if read[i] == 0:
         print(alpha[i], end = " ")
+
+'''
+6 6 5
+D 0
+C 1
+B 2
+B 2
+A 2
+F 4
+
+C 1
+A B C D E F
+x x x     x
+
+B 2
+A B C D E F
+x x       x
+
+A 2
+A B C D E F
+x         x
+'''

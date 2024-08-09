09335002a068bbf318be055730ea70ff0d0819c2
jelly <sohyunzzq@naver.com>
2024-08-10 02:40:45 +0900

    Title: ABC 줄 세우기, Time: 105ms, Memory: 24MB, Status: Passed - CodeTree

--- 240810/ABC 줄 세우기/abc-line-up.py
+++ 240810/ABC 줄 세우기/abc-line-up.py
@@ -9,20 +9,10 @@ for i in range(n): #알파벳
     
     for j in range(n):
         if lst[j] == a:
-            index = j
+            index = j #잘못된 자리에 있음
     
     for j in range(index - i):
         lst[index-j], lst[index-j-1] = lst[index-j-1], lst[index-j]
         cnt += 1
 
 print(cnt)
-'''
-A B C D
-D B A C i: 0, j: 2 / 1 2
-D A B C i: 0,      / 0 1
-A D B C
-
-A B D C i: 1, j: 2 / 1 2
-
-A B C D i: 2, j: 3 / 2 3
-'''

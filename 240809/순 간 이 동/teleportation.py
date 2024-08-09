289ef790ff7b93e42ea1470261d236135c47e597
jelly <sohyunzzq@naver.com>
2024-08-09 22:36:26 +0900

    Title: 순 간 이 동, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240809/순 간 이 동/teleportation.py
@@ -0,0 +1,11 @@
+a, b, x, y = map(int, input().split())
+
+#a - b          :3 - 10
+#a - x - y - b  :3 - 2/8 - 10
+#a - y - x - b  :3 - 8/2 - 10
+
+ans1 = b - a
+ans2 = abs(x-a) + abs(b-y)
+ans3 = abs(x-b) + abs(a-y)
+
+print(min(ans1, ans2, ans3))

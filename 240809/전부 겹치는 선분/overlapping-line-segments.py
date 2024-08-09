81387de0065b86f591fe2cbfb52070262a4dc201
jelly <sohyunzzq@naver.com>
2024-08-09 15:29:21 +0900

    Title: 전부 겹치는 선분, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 240809/전부 겹치는 선분/overlapping-line-segments.py
+++ 240809/전부 겹치는 선분/overlapping-line-segments.py
@@ -7,7 +7,7 @@ def overlap():
             return False
         else:
             line1[0] = max(line1[0], line2[0])
-            line2[1] = min(line1[1], line2[1])
+            line1[1] = min(line1[1], line2[1])
     return True
 
 n = int(input())

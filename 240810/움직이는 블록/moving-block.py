236e5f893186ef87b0c477163cfc844591a890b2
jelly <sohyunzzq@naver.com>
2024-08-10 15:25:33 +0900

    Title: 움직이는 블록, Time: 143ms, Memory: 25MB, Status: Passed - CodeTree

--- 240810/움직이는 블록/moving-block.py
+++ 240810/움직이는 블록/moving-block.py
@@ -3,13 +3,11 @@ lst = []
 for i in range(n):
     lst.append(int(input()))
 
-goal = sum(lst) / n
+goal = sum(lst) // n
 ans = 0
 
 for i in lst:
     if i < goal:
         ans += goal - i
-    elif i > goal:
-        ans += i - goal
 
-print(int(ans // 2))
+print(ans)

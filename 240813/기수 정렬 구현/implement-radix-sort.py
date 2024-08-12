7a2ae9c081d96fe8c5f3978e439708b9f879d601
jelly <sohyunzzq@naver.com>
2024-08-13 02:02:30 +0900

    Title: 기수 정렬 구현, Time: 325ms, Memory: 54MB, Status: Passed - CodeTree

--- 240813/기수 정렬 구현/implement-radix-sort.py
+++ 240813/기수 정렬 구현/implement-radix-sort.py
@@ -1,16 +1,22 @@
 n = int(input())
-lst = list(input().split())
+lst = list(map(int, input().split()))
 
 maxi = max(lst)
-leng = len(maxi)
+leng = len(str(maxi))
 
-for i in range(leng-1, -1, -1):
+for i in range(n):
+    lst[i] = str(lst[i])
+
+for i in range(1, leng+1):
     arr = []
     for j in range(10):
         arr.append([])
     
     for j in range(n):
-        num = int(lst[j][i])
+        if i > len(lst[j]):
+            num = 0
+        else:
+            num = int(lst[j][-i])
         arr[num].append(lst[j])
     
     tmp = []

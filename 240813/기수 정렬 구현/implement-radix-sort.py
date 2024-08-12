0f8741ef64a6328552e34fb648dce6bb54cb4e27
jelly <sohyunzzq@naver.com>
2024-08-13 02:03:01 +0900

    Title: 기수 정렬 구현, Time: 350ms, Memory: 54MB, Status: Passed - CodeTree

--- 240813/기수 정렬 구현/implement-radix-sort.py
+++ 240813/기수 정렬 구현/implement-radix-sort.py
@@ -28,22 +28,3 @@ for i in range(1, leng+1):
 
 for i in range(n):
     print(int(lst[i]), end = " ")
-
-'''
-
-function radix_sort(arr, k)
-  for pos = k - 1 ... pos >= 0:
-    set arr_new = [10][]
-    for i = 0 ... i < arr.size
-      set digit = posth digit of arr[i]
-      arr_new[digit].append(arr[i])
-
-    set store_arr = []
-    for i = 0 ... i < 10
-      for j = 0 ... j < arr_new[i].size
-        store_arr.append(arr_new[i][j])
-  
-    arr = store_arr
-
-  return arr
-'''

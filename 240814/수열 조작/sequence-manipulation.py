949efd5fceb22f3c84d3e52831343298d2a811ec
jelly <sohyunzzq@naver.com>
2024-08-14 19:23:19 +0900

    Title: 수열 조작, Time: 176ms, Memory: 34MB, Status: Passed - CodeTree

--- 240814/수열 조작/sequence-manipulation.py
+++ 240814/수열 조작/sequence-manipulation.py
@@ -2,11 +2,11 @@ from collections import deque
 
 dq = deque()
 n = int(input())
-for i in range(n):
+for i in range(1, n+1):
     dq.append(i)
 
 while len(dq) != 1:
     dq.popleft() #앞에서 하나 빼서 없애기
-    dq.appendleft(dq.popleft()) #앞에서 하나 뺀 다음 뒤로 넣기
-    
+    dq.append(dq.popleft()) #앞에서 하나 뺀 다음 뒤로 넣기
+
 print(dq.pop())

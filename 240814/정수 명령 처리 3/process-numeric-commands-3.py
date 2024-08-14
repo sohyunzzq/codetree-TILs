16447afedacdf87149a178f46535568a1a6b82cd
jelly <sohyunzzq@naver.com>
2024-08-14 19:18:50 +0900

    Title: 정수 명령 처리 3, Time: 333ms, Memory: 29MB, Status: Passed - CodeTree

--- 240814/정수 명령 처리 3/process-numeric-commands-3.py
+++ 240814/정수 명령 처리 3/process-numeric-commands-3.py
@@ -1,36 +1,7 @@
 from collections import deque
 
-class Queue:
-    def __init__(self):
-        self.dq = deque()
+dq = deque()
 
-    def push_front(self, item):
-        self.dq.appendleft(item)
-    
-    def push_back(self, item):
-        self.dq.append(item)
-    
-    def empty(self):
-        if not self.dq:
-            return 1
-        return 0
-    
-    def pop_front(self):
-        return self.dq.popleft()
-    
-    def pop_back(self):
-        return self.dq.pop()
-
-    def size(self):
-        return len(self.dq)
-    
-    def front(self):
-        return self.dq[0]
-
-    def back(self):
-        return self.dq[-1]
-
-dq = Queue()
 n = int(input())
 for i in range(n):
     cmd = list(input().split())
@@ -39,18 +10,21 @@ for i in range(n):
     cmd = cmd[0]
 
     if cmd == "push_front":
-        dq.push_front(num)
+        dq.appendleft(num)
     elif cmd == "push_back":
-        dq.push_back(num)
+        dq.append(num)
     elif cmd == "pop_front":
-        print(dq.pop_front())
+        print(dq.popleft())
     elif cmd == "pop_back":
-        print(dq.pop_back())
+        print(dq.pop())
     elif cmd == "size":
-        print(dq.size())
+        print(len(dq))
     elif cmd == "empty":
-        print(dq.empty())
+        if not dq:
+            print(1)
+        else:
+            print(0)
     elif cmd == "front":
-        print(dq.front())
+        print(dq[0])
     else:
-        print(dq.back())
+        print(dq[-1])

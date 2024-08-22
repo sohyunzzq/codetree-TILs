6808d36ce85e51c724d29d5ffdb712a6faace9b6
jelly <sohyunzzq@naver.com>
2024-08-22 18:09:49 +0900

    Title: 행복한 수열의 개수, Time: 133ms, Memory: 25MB, Status: Passed - CodeTree

--- 240822/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240822/행복한 수열의 개수/number-of-happy-sequence.py
@@ -1,14 +1,17 @@
-def row(i, j):
-    for k in range(m):
-        if lst[i][j] != lst[i][j+k]:
-            return False
-    return True
-
-def col(i, j):
-    for k in range(m):
-        if lst[j][i] != lst[j+k][i]:
-            return False
-    return True
+def check(seq):
+    cnt = 1
+    max_cnt = 1
+    for i in range(1, n):
+        if seq[i-1] == seq[i]:
+            cnt += 1
+        else:
+            cnt = 1
+        
+        max_cnt = max(max_cnt, cnt)
+    
+    if max_cnt >= m:
+        return True
+    return False
 
 n, m = map(int, input().split())
 lst = []
@@ -16,20 +19,19 @@ for i in range(n):
     lst.append(list(map(int, input().split())))
 
 ans = 0
-for i in range(n):
-    check = False
-    for j in range(n-m+1):
-        if not check:
-            if row(i, j):
-                check = True
-                ans += 1
 
+
+#가로를 체크
 for i in range(n):
-    check = False
-    for j in range(n-m+1):
-        if not check:
-            if col(i, j):
-                check = True
-                ans += 1
+    if check(lst[i]):
+        ans += 1
+
+#세로를 체크
+for j in range(n):
+    tmp = []
+    for i in range(n):
+        tmp.append(lst[i][j])
+    if check(tmp):
+        ans += 1
 
 print(ans)

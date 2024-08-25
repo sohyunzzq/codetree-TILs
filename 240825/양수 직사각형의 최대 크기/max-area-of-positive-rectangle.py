f5f61c3c754fd55cd24b8309b38db18f280b8e4c
jelly <sohyunzzq@naver.com>
2024-08-25 16:31:08 +0900

    Title: 양수 직사각형의 최대 크기, Time: 115ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240825/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
+++ 240825/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
@@ -2,29 +2,17 @@ def check(x1, y1, x2, y2):
     for i in range(x1, x2+1):
         for j in range(y1, y2+1):
             if area[i][j] < 0:
-                return False
-    return True
-
-def get_sum(x1, y1, x2, y2):
-    sum1 = 0
-    for i in range(x1, x2+1):
-        for j in range(y1, y2+1):
-            sum1 += area[i][j]
-    return sum1
+                return 0
+    return (x2 - x1 + 1) * (y2 - y1 + 1)
 
 def rectangle():
     maxi = 0
-    ans = 0
     for x1 in range(n):
         for y1 in range(m):
             for x2 in range(x1, n):
                 for y2 in range(y1, m):
-                    if check(x1, y1, x2, y2):
-                        tmp = get_sum(x1, y1, x2, y2)
-                        if tmp > maxi:
-                            maxi = tmp
-                            ans = (x2 - x1 + 1) * (y2 - y1 + 1)
-    return ans
+                    maxi = max(maxi, check(x1, y1, x2, y2))
+    return maxi
 
 n, m = map(int, input().split())
 area = []

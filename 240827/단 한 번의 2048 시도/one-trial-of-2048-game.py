83c4241f92bff4893947b0820c9721473f251252
jelly <sohyunzzq@naver.com>
2024-08-27 21:47:41 +0900

    Title: 단 한 번의 2048 시도, Time: 139ms, Memory: 24MB, Status: Passed - CodeTree

--- 240827/단 한 번의 2048 시도/one-trial-of-2048-game.py
+++ 240827/단 한 번의 2048 시도/one-trial-of-2048-game.py
@@ -3,94 +3,60 @@ def slide(dr):
     for i in range(4):
         temp.append([0] * 4)
 
-    if dr == "U":
-        for col in range(4):
-            index = 0
-            for row in range(4):
-                if grid[row][col] > 0:
-                    temp[index][col] = grid[row][col]
-                    index += 1
-    elif dr == "D":
-        for col in range(3, -1, -1):
-            index = 3
-            for row in range(3, -1, -1):
-                if grid[row][col] > 0:
-                    temp[index][col] = grid[row][col]
-                    index -= 1
-
-    elif dr == "R":
+    for col in range(3, -1, -1):
+        index = 3
         for row in range(3, -1, -1):
-            index = 3
-            for col in range(3, -1, -1):
-                if grid[row][col] > 0:
-                    temp[row][index] = grid[row][col]
-                    index -= 1
-
-    else:
-        for row in range(4):
-            index = 0
-            for col in range(4):
-                if grid[row][col] > 0:
-                    temp[row][index] = grid[row][col]
-                    index += 1
+            if grid[row][col] > 0:
+                temp[index][col] = grid[row][col]
+                index -= 1
 
     return temp    
 
 def combine(dr):
-    if dr == "U":
-        for col in range(4):
-            for i in range(3):
-                if grid[i][col] == 0:
-                    continue
+    for col in range(4):
+        for i in range(3, 0, -1):
+            if grid[i][col] == 0:
+                continue
 
-                if grid[i][col] == grid[i+1][col]:
-                    grid[i][col] *= 2
-                    grid[i+1][col] = 0
+            if grid[i][col] == grid[i-1][col]:
+                grid[i][col] *= 2
+                grid[i-1][col] = 0
 
-    elif dr == "D":
-        for col in range(4):
-            for i in range(3, 0, -1):
-                if grid[i][col] == 0:
-                    continue
+    return grid
 
-                if grid[i][col] == grid[i-1][col]:
-                    grid[i][col] *= 2
-                    grid[i-1][col] = 0
+def rotate(dr, grid):
+    for _ in range(dr):
+        new_grid = []
+        for i in range(4):
+            new_grid.append([0] * 4)
+    
+        for i in range(4):
+            for j in range(4):
+                new_grid[i][j] = grid[3-j][i]
 
-    elif dr == "R":
-        for row in range(4):
-            for i in range(3, 0, -1):
-                if grid[row][i] == 0:
-                    continue
-
-                if grid[row][i] == grid[row][i-1]:
-                    grid[row][i] *= 2
-                    grid[row][i-1] = 0
-
-    else:
-        for row in range(4):
-            for i in range(3):
-                if grid[row][i] == 0:
-                    continue
-
-                if grid[row][i] == grid[row][i+1]:
-                    grid[row][i] *= 2
-                    grid[row][i+1] = 0
+        grid = new_grid
 
     return grid
 
 
 
-
+dict1 = {"D": 0, "L": 3, "U": 2, "R": 1}
 
 grid = []
 for i in range(4):
     grid.append(list(map(int, input().split())))
-dr = input()
+dr = dict1[input()]
 
+#항상 아래로만 떨어지게 하기
+#시계 방향으로 회전시킨 후 떨구고, 다시 돌려서 출력하기
+
+#dict1[dr]만큼 시계 방향으로 회전시키고 떨구기
+
+grid = rotate(dr, grid)
 grid = slide(dr)
 grid = combine(dr)
 grid = slide(dr)
+grid = rotate(4-dr, grid)
 
 
 for row in grid:

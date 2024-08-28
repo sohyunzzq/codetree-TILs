98374d44c41dc7253a425ad79005057930229661
jelly <sohyunzzq@naver.com>
2024-08-28 20:25:03 +0900

    Title: 떨어지는 1자 블록, Time: 141ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240828/떨어지는 1자 블록/falling-horizontal-block.py
@@ -0,0 +1,29 @@
+def max_row():
+    for row in range(n):
+        for col in range(k, k+m):
+            if grid[row][col] == 1:
+                return row - 1
+    return n - 1
+
+
+
+
+
+
+n, m, k = map(int, input().split())
+grid = []
+for i in range(n):
+    grid.append(list(map(int, input().split())))
+
+#m이 크기, k가 블럭의 첫 번째 칸 위치
+
+k -= 1
+row = max_row()
+
+for col in range(k, k+m):
+    grid[row][col] = 1
+
+for row in grid:
+    for col in row:
+        print(col, end = " ")
+    print()

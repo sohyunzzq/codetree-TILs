a258efec2cc01f03948840dc0ed94f8ef17f0388
jelly <sohyunzzq@naver.com>
2024-09-01 18:51:27 +0900

    Title: 쌓인 숫자의 순차적 이동, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240901/쌓인 숫자의 순차적 이동/sequential-movement-of-stacked-numbers.py
@@ -0,0 +1,68 @@
+def in_range(x, y):
+    if 0 <= x < n and 0 <= y < n:
+        return True
+    return False
+
+
+def move(num, x, y):
+    global grid
+
+    ### 위부터 시계 방향
+    dx, dy = [-1, -1, 0, 1, 1, 1, 0, -1], [0, 1, 1, 1, 0, -1, -1, -1]
+
+    maxi = -1
+    maxx, maxy = -1, -1
+    for dr in range(8):
+        nx, ny = x + dx[dr], y + dy[dr]
+        if not in_range(nx, ny):
+            continue
+        
+        temp = grid[nx * n + ny]
+        if temp == []:
+            continue
+        if max(temp) > maxi:
+            maxi = max(temp)
+            maxx, maxy = nx, ny
+    
+    ### 갈 곳이 있는지 확인
+    if maxi != -1:
+
+        ### 지금 내가 있는 리스트에서 제거
+        index = 0
+        while grid[x * n + y][0] != num:
+            grid[maxx * n + maxy].insert(index, grid[x * n + y].pop(0))
+            index += 1
+
+        grid[x * n + y].remove(num)
+        grid[maxx * n + maxy].insert(index, num)
+    
+
+def simul(num):
+    global grid
+
+    for i in range(n * n):
+        if num in grid[i]:
+            move(num, i // n, i % n)
+
+
+
+n, m = map(int, input().split())
+grid = []
+for i in range(n):
+    temp = list(map(int, input().split()))
+    for j in range(n):
+        grid.append([temp[j]])
+nums = list(map(int, input().split()))
+
+
+for i in range(m):
+    simul(nums[i])
+
+for i in range(n * n):
+    if grid[i] == []:
+        print("None", end = " ")
+    else:
+        for item in grid[i]:
+            print(item, end = " ")
+
+    print()

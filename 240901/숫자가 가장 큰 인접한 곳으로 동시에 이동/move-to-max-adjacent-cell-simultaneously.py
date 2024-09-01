017c97ffcbd3dddfe5b1803f2b59410528af3124
jelly <sohyunzzq@naver.com>
2024-09-01 16:27:41 +0900

    Title: 숫자가 가장 큰 인접한 곳으로 동시에 이동, Time: 133ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240901/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.py
@@ -0,0 +1,66 @@
+def in_range(x, y):
+    if 0 <= x < n and 0 <= y < n:
+        return True
+    return False
+
+
+def move():
+    new_count = []
+    for i in range(n):
+        new_count.append([0] * n)
+
+    ### 구슬이 있으면 nc에 다음 위치를 표시해주기
+    for row in range(n):
+        for col in range(n):
+            if count[row][col] == 1:
+                maxi = grid[row][col]
+                maxx, maxy = row, col
+                for dr in range(4):
+                    nx, ny = row + dx[dr], col + dy[dr]
+                    if not in_range(nx, ny):
+                        continue
+                    
+                    if grid[nx][ny] > maxi:
+                        maxi = grid[nx][ny]
+                        maxx, maxy = nx, ny
+                
+                new_count[maxx][maxy] += 1
+                        
+
+    
+    ### 구슬 충돌한 거 삭제
+    for row in range(n):
+        for col in range(n):
+            if new_count[row][col] >= 2:
+                new_count[row][col] = 0
+    
+    ### 배열 카피
+    for row in range(n):
+        for col in range(n):
+            count[row][col] = new_count[row][col]
+
+
+####### 입력받기
+n, m, t = map(int, input().split())
+grid = []
+count = []
+for i in range(n):
+    grid.append(list(map(int, input().split())))
+    count.append([0] * n)
+for i in range(m):
+    x, y = map(int, input().split())
+    count[x-1][y-1] = 1
+
+
+####### 기본 세팅
+dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
+
+
+####### 시뮬
+for i in range(t):
+    move()
+
+ans = 0
+for row in range(n):
+    ans += sum(count[row])
+print(ans)

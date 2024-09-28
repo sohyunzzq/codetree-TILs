2d5e5108e73a638300e5f0da8cde5a015b4247b9
jelly <sohyunzzq@naver.com>
2024-09-28 22:17:08 +0900

    Title: 겹치지 않게 선분 고르기, Time: 98ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240928/겹치지 않게 선분 고르기/select-segments-without-overlap.py
+++ 240928/겹치지 않게 선분 고르기/select-segments-without-overlap.py
@@ -2,31 +2,14 @@ n = int(input())
 line = []
 for i in range(n):
     line.append(list(map(int, input().split())))
+line.sort(key = lambda x: (-x[1], -x[0]))
 
-lst = []
-maxi = 0
+cnt = 1
 
-def select(pos):
-    global maxi
-    if pos == n:
-        return
+last = line[0]
+for i in range(n-1):
+    if last[0] > line[i+1][1]:
+        cnt += 1
+        last = line[i+1]
 
-    for i in range(n):
-        lst.append(line[i])
-        if check():
-            maxi = max(maxi, len(lst))
-        select(pos + 1)
-        lst.pop()
-
-def check():
-    lst.sort(key = lambda x: x[0])
-    
-    for i in range(1, len(lst)):
-        if lst[i][0] <= lst[i-1][1]:
-            return False
-    
-    return True
-
-select(0)
-
-print(maxi)
+print(cnt)

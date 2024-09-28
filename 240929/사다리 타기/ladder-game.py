f4727f23af280bb67882300106bec8fc8cdb6c2d
jelly <sohyunzzq@naver.com>
2024-09-29 00:12:34 +0900

    Title: 사다리 타기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240929/사다리 타기/ladder-game.py
@@ -0,0 +1,48 @@
+#38분
+
+#사다리 타기 알고리즘
+#라인들을 위에서부터 오도록 정렬하고
+#가로선을 만나면 그거에 해당하는 값만 swap하면 된다
+
+lst = []
+def select(index):
+    global mini
+
+    if index == m:
+        if game():
+            mini = min(mini, len(lst))
+        return
+
+    lst.append(line[index])
+    select(index + 1)
+    lst.pop()
+
+    select(index + 1)
+
+
+
+def game():
+    originalans = [1, 2, 3, 4]
+    ans = [1, 2, 3, 4]
+
+    for item in line:
+        originalans[item[0] - 1], originalans[item[0]] = originalans[item[0]], originalans[item[0] - 1]
+    
+    for item in lst:
+        ans[item[0] - 1], ans[item[0]] = ans[item[0]], ans[item[0] - 1]
+
+    if originalans == ans:
+        return True
+    return False
+
+
+n, m = map(int, input().split())
+line = []
+for i in range(m):
+    line.append(list(map(int, input().split())))
+line.sort(key = lambda x: x[1])
+
+mini = m
+select(0)
+
+print(mini)

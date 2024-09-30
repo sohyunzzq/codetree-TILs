75afaf3155782e944994764e8d2228382209ade0
jelly <sohyunzzq@naver.com>
2024-09-30 17:08:53 +0900

    Title: 계단 오르기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240930/계단 오르기/climbing-stairs.py
+++ 240930/계단 오르기/climbing-stairs.py
@@ -2,11 +2,11 @@ n = int(input())
 
 dp = [0] * (n + 1)
 dp[0] = 1
+dp[1] = 0
+dp[2] = 1
+dp[3] = 1
 
-for i in range(2, n + 1):
-    if i >= 2:
-        dp[i] += dp[i-2]
-    if i >= 3:
-        dp[i] += dp[i-3]
+for i in range(4, n + 1):
+    dp[i] = dp[i-2] + dp[i-3]
 
 print(dp[n] % 10007)

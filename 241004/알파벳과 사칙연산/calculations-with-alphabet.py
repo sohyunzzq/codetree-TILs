9200cd5b942662cfde85eeee2950807305d123c8
jelly <sohyunzzq@naver.com>
2024-10-04 19:28:18 +0900

    Title: 알파벳과 사칙연산, Time: 176ms, Memory: 26MB, Status: Wrong Answer - CodeTree

---
+++ 241004/알파벳과 사칙연산/calculations-with-alphabet.py
@@ -0,0 +1,49 @@
+string = input()
+
+alphas = set()
+
+for i in range(len(string)):
+    if i % 2 == 0:
+        alphas.add(string[i])
+
+alphas = list(alphas)
+lst = []
+matched = {}
+
+maxi = 0
+
+def get_value():
+    ans = matched[string[0]]
+
+    for i in range(1, len(string), 2):
+        oper = string[i]
+        alpha = matched[string[i + 1]]
+
+        if oper == "+":
+            ans += alpha
+        elif oper == "-":
+            ans -= alpha
+        else:
+            ans *= alpha
+    
+    return ans
+
+def assign():
+    for i in range(len(alphas)):
+        matched[alphas[i]] = lst[i]
+
+def choose(pos):
+    global maxi
+
+    if pos == len(alphas):
+        assign()
+        maxi = max(maxi, get_value())
+        return
+
+    for i in range(1, 5):
+        lst.append(i)
+        choose(pos + 1)
+        lst.pop()
+
+choose(0)
+print(maxi)

a9348a6b23c8882f1c8975900ab08d942dc6f862
jelly <sohyunzzq@naver.com>
2024-10-08 00:50:05 +0900

    Title: 원점으로부터의 거리, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241008/원점으로부터의 거리/distance-from-origin.py
@@ -0,0 +1,19 @@
+class coor:
+    def __init__(self, x, y, num):
+        self.x = x
+        self.y = y
+        self.num = num
+
+n = int(input())
+
+coors = []
+for i in range(n):
+    x, y = map(int, input().split())
+    c = coor(x, y, i+1)
+
+    coors.append(c)
+
+coors.sort(key = lambda x: (abs(x.x + x.y), x.num))
+
+for c in coors:
+    print(c.num)

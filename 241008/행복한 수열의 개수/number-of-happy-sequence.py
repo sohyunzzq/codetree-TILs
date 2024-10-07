93267e382fc3819073528307d088ff9f99d3594b
jelly <sohyunzzq@naver.com>
2024-10-08 02:11:10 +0900

    Title: 행복한 수열의 개수, Time: 166ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 241008/행복한 수열의 개수/number-of-happy-sequence.py
@@ -0,0 +1,33 @@
+n, m = map(int, input().split())
+area = []
+for i in range(n):
+    area.append(list(map(int, input().split())))
+
+def happy_row(area, num):
+    for i in range(m):
+        if area[row][col + i] != num:
+            return False
+    return True
+        
+cnt = 0
+for row in range(n):
+    for col in range(n-m+1):
+        if happy_row(area, area[row][col]):
+            cnt += 1
+            break
+
+lst2 = []
+for i in range(n):
+    lst2.append([0] * n)
+
+for row in range(n):
+    for col in range(n):
+        lst2[row][col] = area[col][row]
+
+for row in range(n):
+    for col in range(n-m+1):
+        if happy_row(lst2, area[row][col]):
+            cnt += 1
+            break
+
+print(cnt)

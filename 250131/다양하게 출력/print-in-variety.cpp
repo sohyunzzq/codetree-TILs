f02837449a84b6648a35b6be913bd45217c4beae
jelly <sohyunzzq@naver.com>
2025-01-31 18:42:59 +0900

    Title: 다양하게 출력, Time: 2ms, Memory: 0MB, Status: Passed - CodeTree

--- 250131/다양하게 출력/print-in-variety.cpp
+++ 250131/다양하게 출력/print-in-variety.cpp
@@ -5,7 +5,7 @@ int main() {
     cout << "Total days in Year" << endl;
     cout << 365 << endl;
     cout << "Circumference rate" << endl;
-    cout << 3.1415926535;
+    cout << "3.1415926535";
     // Please write your code here.
     return 0;
 }

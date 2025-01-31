7d95bc499eab61bb82f310ca30092ee59c7d1a67
jelly <sohyunzzq@naver.com>
2025-01-31 19:43:18 +0900

    Title: 변수 값 교체하기 6, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250131/변수 값 교체하기 6/replacing-variable-values-6.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a=2, b=5;
+    int tmp=a;
+    a=b;
+    b=tmp;
+
+    cout<<a<<endl<<b;
+    // Please write your code here.
+    return 0;
+}

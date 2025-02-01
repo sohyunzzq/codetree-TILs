ba4d67e10a24552e43ff527c5811f05623d7e096
jelly <sohyunzzq@naver.com>
2025-02-01 13:13:04 +0900

    Title: 입력받은 값 교체하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250201/입력받은 값 교체하기/changing-inputs.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin>>a>>b;
+    int tmp = a;
+    a=b;
+    b=tmp;
+    cout<<a<<" "<<b;
+    // Please write your code here.
+    return 0;
+}

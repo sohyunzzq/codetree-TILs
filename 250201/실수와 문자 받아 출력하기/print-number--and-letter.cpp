7f42c728a2212d30560b79f2672da707ba512bf5
jelly <sohyunzzq@naver.com>
2025-02-01 13:26:14 +0900

    Title: 실수와 문자 받아 출력하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250201/실수와 문자 받아 출력하기/print-number--and-letter.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char c;
+    double a, b;
+    cin>>c>>a>>b;
+    cout<<fixed;
+    cout.precision(2);
+    cout<<c<<endl<<a<<b;
+    // Please write your code here.
+    return 0;
+}

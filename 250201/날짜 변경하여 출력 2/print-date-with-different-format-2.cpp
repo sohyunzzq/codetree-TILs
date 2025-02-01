ed2748bb8136650c4a5751ed52d00cd0d67e6fe1
jelly <sohyunzzq@naver.com>
2025-02-01 13:36:37 +0900

    Title: 날짜 변경하여 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250201/날짜 변경하여 출력 2/print-date-with-different-format-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int m, d, y;
+    cin>>m;
+    cin.get();
+    cin>>d;
+    cin.get();
+    cin>>y;
+
+    cout<<y<<"."<<m<<"."<<d;
+    // Please write your code here.
+    return 0;
+}

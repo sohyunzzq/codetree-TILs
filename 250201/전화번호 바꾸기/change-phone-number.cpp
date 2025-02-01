f5db97476bf13f3106d11c814fdb9207bee0c84a
jelly <sohyunzzq@naver.com>
2025-02-01 13:47:28 +0900

    Title: 전화번호 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250201/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    int c;
+    cin>>c;
+    cin.get();
+    cin>>a;
+    cin.get();
+    cin>>b;
+    cout<<"010-"<<b<<"-"<<a;
+    
+    // Please write your code here.
+    return 0;
+}

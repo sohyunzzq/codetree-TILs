a8844c3a930b1c71cf84aaa27f4d2d8b6780aeb1
jelly <sohyunzzq@naver.com>
2025-02-03 10:45:58 +0900

    Title: 물의 상태, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250203/물의 상태/state-of-water.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+
+    if (n<0)
+    cout<<"ice";
+    else if (n>=100)
+    cout<<"vapor";
+    else
+    cout<<"water";
+    // Please write your code here.
+    return 0;
+}

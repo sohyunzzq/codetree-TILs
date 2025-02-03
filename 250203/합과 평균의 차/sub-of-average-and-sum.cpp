2397fae6385ce4e6cf4e24bfd4e9ae7332d55932
jelly <sohyunzzq@naver.com>
2025-02-03 09:38:38 +0900

    Title: 합과 평균의 차, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250203/합과 평균의 차/sub-of-average-and-sum.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin>>a>>b>>c;
+
+    cout<<a+b+c<<endl<<double(a+b+c)/3<<endl<<(a+b+c)-double(a+b+c)/3;
+    // Please write your code here.
+    return 0;
+}

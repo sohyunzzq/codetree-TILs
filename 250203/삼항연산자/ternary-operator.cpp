1e5a673b586a49becb2cfd73d9ff7b0f3b5b2c33
jelly <sohyunzzq@naver.com>
2025-02-03 10:33:52 +0900

    Title: 삼항연산자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250203/삼항연산자/ternary-operator.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin>>a;
+
+    if (a==100)
+    cout<<"pass";
+    else
+    cout<<"failure";
+    // Please write your code here.
+    return 0;
+}

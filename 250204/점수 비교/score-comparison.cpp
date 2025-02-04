a249aa7479ed1f332cff0799f187bc336695c471
jelly <sohyunzzq@naver.com>
2025-02-04 12:46:30 +0900

    Title: 점수 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250204/점수 비교/score-comparison.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a1, a2;
+    int b1, b2;
+
+    cin>>a1>>a2>>b1>>b2;
+
+    cout << (a1>b1 && a2>b2);
+    // Please write your code here.
+    return 0;
+}

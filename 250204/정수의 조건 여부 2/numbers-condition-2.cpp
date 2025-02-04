36b331b968f034916c9d35c59856a92b9c8e90b9
jelly <sohyunzzq@naver.com>
2025-02-04 12:37:34 +0900

    Title: 정수의 조건 여부 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250204/정수의 조건 여부 2/numbers-condition-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin>>a;
+
+    if(a==5)
+        cout<<"A";
+    else if(a%2==0)
+        cout<<"B";
+    // Please write your code here.
+    return 0;
+}

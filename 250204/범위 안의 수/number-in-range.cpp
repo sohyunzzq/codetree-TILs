91586124b5176446940beb931dff2a32237aec40
jelly <sohyunzzq@naver.com>
2025-02-04 12:47:21 +0900

    Title: 범위 안의 수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250204/범위 안의 수/number-in-range.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin>>a;
+
+    if (10<=a && a<=20)
+    cout<<"yes";
+    else
+    cout<<"no";
+    // Please write your code here.
+    return 0;
+}

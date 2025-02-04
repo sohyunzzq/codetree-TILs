06c7641e248f9b162029828be88c91b34061727d
jelly <sohyunzzq@naver.com>
2025-02-04 12:41:32 +0900

    Title: 3 또는 5의 배수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250204/3 또는 5의 배수/multiples-of-3-or-5.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin>>a;
+
+    if(a%3==0)
+    cout<<"YES";
+    else
+    cout<<"NO";
+    cout<<endl;
+    if(a%5==0)
+    cout<<"YES";
+    else
+    cout<<"NO";
+    
+    // Please write your code here.
+    return 0;
+}

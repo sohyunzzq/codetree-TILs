9208e0274bf9c4c6df7e5a8fc5da08fca9ebd604
jelly <sohyunzzq@naver.com>
2025-02-13 19:10:35 +0900

    Title: 19단 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 250213/19단 출력/nineteen-times-table.cpp
+++ 250213/19단 출력/nineteen-times-table.cpp
@@ -6,7 +6,7 @@ int main() {
     for(int i=1; i<=19; i++){
         for(int j=1; j<=19; j++) {
             cout <<i<<" * "<<j<<" = "<<i*j;
-            if (j%2==0)
+            if (j%2==0 || j==19)
             cout<<endl;
             else
             cout<<" / ";

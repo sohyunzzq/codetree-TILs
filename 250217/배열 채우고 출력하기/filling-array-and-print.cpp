9856054affd393f4540b2d46bc76c968797eafd2
jelly <sohyunzzq@naver.com>
2025-02-17 18:32:10 +0900

    Title: 배열 채우고 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250217/배열 채우고 출력하기/filling-array-and-print.cpp
+++ 250217/배열 채우고 출력하기/filling-array-and-print.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 
 int main() {
-    int arr[10];
+    char arr[10];
     for(int i=0; i<10; i++)
     cin>>arr[i];
 
     for(int i=9; i>=0; i--)
-    cout<<arr[i] <<" ";
+    cout<<arr[i];
     // Please write your code here.
     return 0;
 }

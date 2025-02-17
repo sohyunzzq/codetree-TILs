9948c17ff905af0587e9ff86f1ee69430ce99f06
jelly <sohyunzzq@naver.com>
2025-02-17 18:41:13 +0900

    Title: 특정 수 도달하기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250217/특정 수 도달하기/reaching-specific-number.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int sum=0;
+    int index=10;
+    for(int i=0; i<10; i++){
+        int n;
+        cin>>n;
+
+        if(n>=250){
+            index =i;
+        break;
+        }
+
+        sum+=n;
+    }
+
+    cout<<sum<<" "<<(double)sum/index;
+    // Please write your code here.
+    return 0;
+}

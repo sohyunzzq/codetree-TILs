58447daf94830e4b92ac7117e72b7112dac85d67
jelly <sohyunzzq@naver.com>
2025-02-21 19:18:21 +0900

    Title: 연속부분수열일까, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250221/연속부분수열일까/contiguous-array-or-not.cpp
+++ 250221/연속부분수열일까/contiguous-array-or-not.cpp
@@ -11,22 +11,21 @@ int main() {
     for(int i=0; i<n2; i++)
     cin>>arr2[i];
 
-    int flag=1;
+    int flag=0;
 
     if (n1>=n2){
         for(int i=0; i<=n1-n2; i++) {
             if (arr1[i]==arr2[0]) {
                 for(int j=0; j<n2; j++) {
                     if (arr1[i+j] != arr2[j]){
-                    flag=0;
+                        flag=0;
                     break;
                     }
+                    flag=1;
                 }
             }
         }
     }
-    else
-    flag=0;
 
     cout<<(flag?"Yes":"No");
 

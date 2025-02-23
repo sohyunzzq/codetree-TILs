5e502439a55da8328b1edff368e9a3d28b1db291
jelly <sohyunzzq@naver.com>
2025-02-24 07:53:34 +0900

    Title: 999 또는 -999, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 250224/999 또는 -999/999-or-999.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int min_val=1000;
+    int max_val=-1000;
+
+    while(1){
+        int n;
+        cin>>n;
+
+        if(n==999 || b==-999)
+        break;
+
+        if(n>max_val)
+        max_val=n;
+
+        if(n<min_val)
+        min_val=n;
+    }
+
+    cout<<max_val<<" "<<min_val;
+    // Please write your code here.
+    return 0;
+}

ebc08a00c25eb724275a5cc1cfd8f92554f1d7df
jelly <sohyunzzq@naver.com>
2025-02-27 00:30:57 +0900

    Title: 쪼개어 배낭 채우기 구현, Time: 33ms, Memory: 1MB, Status: Wrong Answer - CodeTree

---
+++ 250227/쪼개어 배낭 채우기 구현/implement-fractional-knapsack.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+struct Jewel {
+    double weight;
+    double value;
+    double per;
+    
+    bool operator<(struct Jewel right) {
+        if (per > right.per) return true;
+        if (per < right.per) return false;
+        return false;
+    }
+};
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    vector<struct Jewel> jewels(n);
+    for (int i = 0; i < n; i++) {
+        int w, v;
+        cin >> w >> v;
+        
+        jewels[i].weight = w;
+        jewels[i].value = v;
+        jewels[i].per = v / w;
+    }
+
+    sort(jewels.begin(), jewels.end());
+
+    double ans = 0;
+    double total_weight = 0;
+    for (int i = 0; i < n; i++) {
+        if (jewels[i].weight <= m - total_weight) {
+            total_weight += jewels[i].weight;
+            ans += jewels[i].value;
+        }
+        else {
+            ans += jewels[i].value * ((m - total_weight) / jewels[i].weight);
+            break;
+        }
+
+        if (total_weight >= m)
+            break;
+    }
+
+    cout << fixed;
+    cout.precision(3);
+
+    cout << ans;
+    // Please write your code here.
+    return 0;
+}

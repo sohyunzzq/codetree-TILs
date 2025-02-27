1118ff995b8614d00381fd135f20c7e4e60a136b
jelly <sohyunzzq@naver.com>
2025-02-27 12:05:25 +0900

    Title: 최대 수 만들기, Time: 16ms, Memory: 0MB, Status: Runtime Error - CodeTree

--- 250227/최대 수 만들기/make-biggest-num.cpp
+++ 250227/최대 수 만들기/make-biggest-num.cpp
@@ -1,33 +1,28 @@
 #include <iostream>
 #include <algorithm>
+#include <string>
 using namespace std;
 
 bool cmp(string left, string right) {
-    int min_len = min(left.size(), right.size());
-    for (int i = 0; i < min_len; i++) {
-        if (int(left[i] - '0') >= int(right[i] - '0'))
-            return true;
-        else if (int(left[i] - '0') < int(right[i] - '0'))
-            return false;
-    }
-    if (left.size() < right.size()) {
-        if (right[min_len] > right[0])
-            return true;
-        else
-            return false;
-    }
-    if (left.size() > right.size()) {
-        if (left[min_len] > left[0])
-            return true;
-        else
-            return false;
-    }
+    if (left.size() == right.size())
+        for (int i = 0; i < left.size(); i++)
+            if (left[i] > right[i])
+                return true;
+            else
+                return false;
+    
+    string left_right = left + right;
+    string right_left = right + left;
+
+    if (stoi(left_right) > stoi(right_left))
+        return true;
+    return false;
 }
 
 int main() {
     int n;
     cin >> n;
-    string nums[50000];
+    string nums[500];
     for (int i = 0; i < n; i++)
         cin >> nums[i];
 

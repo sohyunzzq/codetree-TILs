de3f5b0bd1e95bc8c9f6994c0ee72e1223086bf9
jelly <sohyunzzq@naver.com>
2025-03-04 01:30:42 +0900

    Title: 각 정점까지의 최단 경로 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250304/각 정점까지의 최단 경로 3/shortest-path-to-each-vertex-3.cpp
+++ 250304/각 정점까지의 최단 경로 3/shortest-path-to-each-vertex-3.cpp
@@ -13,15 +13,16 @@ struct Node {
 
 int n, m;
 int dist[101];
+bool check[101];
 priority_queue<struct Node> q;
 
 int main() {
 	cin >> n >> m;
 
 	vector<vector<int>> graph;
-	graph.resize(n+1);
+	graph.resize(n + 1);
 	for (int i = 1; i <= n; i++)
-		graph[i].resize(n+1);
+		graph[i].resize(n + 1);
 
 	for (int i = 0; i < m; i++) {
 		int start, end, weight;
@@ -41,6 +42,7 @@ int main() {
 			if (graph[now.v][i]) {
 				if (dist[i] > dist[now.v] + graph[now.v][i]) {
 					dist[i] = dist[now.v] + graph[now.v][i];
+					check[i] = true;
 					q.push({ i, dist[i] });
 				}
 			}
@@ -48,7 +50,7 @@ int main() {
 	}
 
 	for (int i = 2; i <= n; i++)
-		if (dist[i] == INT_MAX)
+		if (!check[i])
 			cout << -1 << endl;
 		else
 			cout << dist[i] << endl;

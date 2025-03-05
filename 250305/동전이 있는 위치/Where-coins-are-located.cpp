c3fa41c1713607c91181184d9ca800136c4490b0
jelly <sohyunzzq@naver.com>
2025-03-05 18:37:50 +0900

    Title: 동전이 있는 위치, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250305/동전이 있는 위치/Where-coins-are-located.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin>>n>>m;
+
+    int area[9][9]={};
+    for(int i=0; i<m; i++) {
+        int x, y;
+        cin>>x>>y;
+
+        area[x-1][y-1]=1;
+    }
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++)
+        cout<<area[i][j]<<" ";
+        cout<<endl;
+    }
+    // Please write your code here.
+    return 0;
+}

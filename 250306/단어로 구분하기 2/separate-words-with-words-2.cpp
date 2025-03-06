4fc0728a0aefe3124eb84dc43732303e810e1f2b
jelly <sohyunzzq@naver.com>
2025-03-06 18:46:01 +0900

    Title: 단어로 구분하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250306/단어로 구분하기 2/separate-words-with-words-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    for(int i=0; i<10; i++){
+        string str;
+        cin>>str;
+
+        if(i%2==0)
+        cout<<str<<endl;
+    }
+    // Please write your code here.
+    return 0;
+}

a29a3badfd5b5342ccca6e972c54a0943de9a5c0
sohyun <sohyunzzq@naver.com>
2025-06-23 00:58:35 +0900

    Title: 특정 문자의 유무, Time: 16ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 250623/특정 문자의 유무/specific-character-presence.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	string s;
+	cin >> s;
+
+	bool ee = false;
+	bool ab = false;
+
+	for (int i = 0; i < s.size() - 2; i++) {
+		if (s[i] == 'e' && s[i + 1] == 'e')
+			ee = true;
+
+		if (s[i] == 'a' && s[i + 1] == 'b')
+			ab = true;
+	}
+
+	if (ee)
+		cout << "Yes ";
+	else
+		cout << "No ";
+
+	if (ab)
+		cout << "Yes";
+	else
+		cout << "No";
+}

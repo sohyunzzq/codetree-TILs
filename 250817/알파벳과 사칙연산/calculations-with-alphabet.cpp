9107e143a2106edc9a698de547cb97e65325de76
jelly <sohyunzzq@naver.com>
2025-08-17 23:56:44 +0900

    Title: 알파벳과 사칙연산, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250817/알파벳과 사칙연산/calculations-with-alphabet.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <map>
+using namespace std;
+
+string s;
+int selected[6];
+vector<int> index;
+int ans;
+int alpha[6];
+
+void GetAns() {
+	int tmp = selected[s[0] - 'a'];
+
+	for (int i = 1; i < s.size(); i += 2) {
+		switch (s[i]) {
+		case '+':
+			tmp += selected[s[i+1] - 'a'];
+			break;
+		case '-':
+			tmp -= selected[s[i + 1] - 'a'];
+			break;
+		case '*':
+			tmp *= selected[s[i + 1] - 'a'];
+			break;
+		}
+	}
+
+	ans = max(ans, tmp);
+}
+
+void func(int idx) {
+	if (idx == index.size()) {
+		GetAns();
+		return;
+	}
+
+	for (int i = 1; i <= 4; i++) {
+		selected[index[idx]] = i;
+		func(idx + 1);
+	}
+}
+
+int main() {
+	cin >> s;
+
+	for (int i = 0; i < s.size(); i += 2) {
+		alpha[s[i] - 'a']++;
+	}
+
+	for (int i = 0; i < 6; i++) {
+		if (alpha[i]) {
+			index.push_back(i);
+		}
+	}
+
+	func(0);
+
+	cout << ans;
+}
